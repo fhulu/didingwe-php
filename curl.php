13b1accdb3fdefb54db280b6edf68941cb4e6153
fhulu <fhulu>
2011-11-15 06:38:07 +0000

    moved from ots

---
+++ curl.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once('log.php');
+
+class curl_exception extends Exception {};
+
+class curl 
+{
+  var $session;
+  function __construct()
+  {
+    $this->session = curl_init();
+  }
+  
+  function __destruct()
+  {
+    curl_close($this->session);
+  }
+    
+  function read($url, $limit=0)
+  { 
+    log::debug("CURL Read $url");
+    curl_setopt($this->session, CURLOPT_URL, $url);
+    if ($limit > 0) 
+	  $range = "0-$limit";
+	else
+	  $range = "0-";
+	curl_setopt ($this->session, CURLOPT_HTTPHEADER, array("Range: $range"));
+    curl_setopt($this->session, CURLOPT_RETURNTRANSFER, 1);
+    
+    return curl_exec($this->session);
+  }
+
+  function download($url, $dest_path=null)
+  { 
+    curl_setopt($this->session, CURLOPT_URL, $url);
+    log::debug("CURL Download $url");
+    if (is_null($dest_path)) $dest_path = "/tmp/" . basename($url);
+    $file = fopen($dest_path, "w");
+    curl_setopt($this->session, CURLOPT_FILE, $file);
+    if (!curl_exec($this->session)) {
+      fclose($file);
+      return null;
+    }
+    fclose($file);  
+    return $dest_path;
+  }
+  
+  function save($url, $dest_path=null)
+  { 
+    return $this->download($url, $dest_path); 
+  }
+
+  function show($url)
+  {
+    echo $this->read($url);
+  }
+
+  function get_last_mime()
+  {
+    return curl_getinfo($this->session, CURLINFO_CONTENT_TYPE);
+  }
+};
+?>
---
+++ image.php
@@ -0,0 +1,33 @@
+<?php
+
+class image_exception extends Exception {};
+
+class image {
+
+  static function resize($path, $width=0, $height=0)
+  {
+    if ($width ==0 && $height == 0) return;
+
+    list($old_w,$old_h) = getimagesize($path);
+    if ($height == 0) {
+      $new_w = $width;
+      $new_h = $old_h * round($new_w/$old_w,2);
+    }
+    if ($width == 0) {
+      $new_h = $height;
+      $new_w = $old_w * round($new_h/$old_h,2);
+    }
+
+    $source = imagecreatefromjpeg($path); // Set the resource for the source image
+    $dest = imagecreatetruecolor($new_w,$new_h); // Create resource for thumbnail
+    imagecopyresized($dest, $source, 0,0,0,0, $new_w, $new_h, $old_w, $old_h);
+
+    $new_path = tempnam("/tmp", "image");
+
+    imagejpeg($dest,$new_path,85); // Stream image to file $new_path
+    imagedestroy($source);
+    imagedestroy($dest);
+    rename($new_path, $path);
+  }
+}
+?>
