142140360a8991dfb77da4c5caaacb7e6394fafd
fhulu <fhulu>
2011-12-10 23:56:07 +0000

    - ajax_inner() now incoporates ajax_inner_progress() - ajax_inner_progress now deprecated

--- ajax.js
+++ ajax.js
@@ -1,5 +1,4 @@
 
-var ajax = null;
 function create_ajax()
 {
   if (ajax != null) return ajax;
@@ -8,22 +7,24 @@ function create_ajax()
   else ajax = new ActiveXObject("Microsoft.XMLHTTP");
   return ajax;
 }
+var ajax = create_ajax();
 
-function ajax_call(ajax, url, async, func)
+function ajax_call(url, async, func)
 {
   ajax.onreadystatechange=func;
   ajax.open("GET", url, async);
   ajax.send();
 }
 
-function ajax_inner_cb(ajax, obj)
+function ajax_inner_cb(obj)
 {
   if (ajax.readyState==4 && ajax.status==200) {
+    //eval(ajax.responseText);
     obj.innerHTML=ajax.responseText;
   }
 }
 
-function ajax_value_cb(ajax, obj)
+function ajax_value_cb(obj)
 {
   if (ajax.readyState==4 && ajax.status==200) {
     obj.value=ajax.responseText;
@@ -35,24 +36,39 @@ function ajax_query(source, url, result_id)
   ajax_inner(result_id, url+"?q="+source.value);
 }
 
-function ajax_inner(result_id, url)
+function ajax_inner(result_id, url, progress_str, progress_id, sync)
 {
+  if (progress_str === true)
+    sync = true;   // progress_str can also act as sync parameter
+  else if (!(progress_str === undefined)) {
+    if (progress_id === undefined) progress_id = result_id;
+    var obj = getElementByIdOrName(progress_id);
+    obj.innerHTML = '<div class=progress>' + progress_str + '.</div>';
+    if (sync === undefined) sync = true;
+  }
+  else
+    sync = false;
+
   var obj = getElementByIdOrName(result_id);
-  var ajax = create_ajax();
-  ajax_call(ajax, url_params(url), true, function() { ajax_inner_cb(ajax, obj); });
+  ajax_call(url_params(url), !sync, function() { ajax_inner_cb(obj); });
 }
 
-function ajax_value(result_id, url)
+function ajax_value(result_id, url, progress_str, progress_id, sync)
 {
-  var ajax = create_ajax();
+  if (!(progress_str === undefined)) {
+    if (progress_id === undefined) progress_id = result_id;
+    var obj = getElementByIdOrName(progress_id);
+    obj.innerHTML = '<div class=progress>' + progress_str + '.</div>';
+    sync = true;
+  }
   var obj = getElementByIdOrName(result_id);
-  ajax_call(ajax, url_params(url), true, function() { ajax_value_cb(ajax, obj); });
+  ajax_call(url_params(url), true, function() { ajax_value_cb(obj); });
 }
 
-function _ajax_confirm(ajax, url)
+function ajax_confirm(url)
 {
   var confirmation;
-  ajax_call(ajax, url_params(url), false, function() { 
+  ajax_call(url_params(url), false, function() { 
     if (ajax.readyState==4 && ajax.status==200) 
       confirmation = ajax.responseText;
   });
@@ -66,15 +82,8 @@ function _ajax_confirm(ajax, url)
   return true;
 }
 
-function ajax_confirm(url)
-{
-  var ajax = create_ajax();
-  return _ajax_confirm(ajax, url);
-}
-
 function ajax_mconfirm(url,params)
 {
-  var ajax = create_ajax();
   var func_idx = url.lastIndexOf('/') + 1;
   var funcs = url.substr(func_idx);
   url = url.substr(0, func_idx);
@@ -82,7 +91,7 @@ function ajax_mconfirm(url,params)
   for (var i=0; i<funcs.length; ++i) {
     var func = funcs[i];
     func += func.indexOf('?') < 0? '?': '&';
-    if (!_ajax_confirm(ajax, url + func + params))
+    if (!ajax_confirm(url + func + params))
       return false;
   }
   return true;
@@ -99,6 +108,5 @@ function ajax_inner_progress(result_id, progress_id, progress_str, url)
   var obj = getElementByIdOrName(progress_id);
   obj.innerHTML = '<div class=progress>' + progress_str + '.</div>';
   obj = getElementByIdOrName(result_id);
-  var ajax = create_ajax();
-  ajax_call(ajax, url_params(url), true, function() { ajax_inner_cb(ajax, obj); });
+  ajax_call(url_params(url), false, function() { ajax_inner_cb(obj); });
 }
