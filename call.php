32575b62856641ffa55af83dbdbc85080c41d941
fhulu <fhulu>
2011-12-11 00:00:31 +0000

    - added exception handling

--- call.php
+++ call.php
@@ -7,7 +7,12 @@ if (!is_null($method)) {
   log::init($class, log::DEBUG);
 }
 foreach($_ENV as $key=>$env) $_SESSION[$key] = $env;
-global $client_id;
 $client_id = $_SESSION[client_id];
-call_user_func_array($argv[1], array_slice($argv, 2));
+try {
+  echo call_user_func_array($argv[1], array_slice($argv, 2));
+}
+catch (Exception $e) {
+  log::error("UNCAUGHT EXCEPTION: " . $e->getMessage() );
+}
+
 ?>
