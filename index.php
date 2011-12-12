810b07cff56ea290dbf24a50d31ba625a2acf748
linden <linden>
2011-12-12 10:07:39 +0000

    Made all div's optional

--- index.php
+++ index.php
@@ -5,13 +5,17 @@ log::init('index', log::DEBUG);
 $_SESSION['referrer'] = $_SERVER[REQUEST_URI];
 
 
-function load_js($div)
+function load_div($div, $page)
 {
-  $paths = explode('/', $_SESSION[$div]);
-  $js = $paths[sizeof($paths)-1].'.js';
+  $paths = explode('/', $page);
+  $args = explode('?', $paths[sizeof($paths)-1]);
+  $file_name = $args[0];
+  $js = "$file_name.js";
   log::debug("JSFILE: $js");
   if (file_exists($js))
     echo "<script type='text/javascript' src='$js'></script>\n"; 
+  if (file_exists("$file_name") || file_exists("$file_name.php") || file_exists("$file_name.html")) 
+    $_SESSION[$div] = "do.php/$page";
 }
 
 function init_div($div, $default=null)
@@ -19,15 +23,10 @@ function init_div($div, $default=null)
   $var = $div[0];
   $page = $_GET[$var]; 
   if ($page == '') {
-    if (!isset($_SESSION[$div])) {
+    if (!isset($_SESSION[$div])) 
       $page = is_null($default)? $div: $default;
-      $_SESSION[$div] = "do.php/$page";
-    }
   }
-  else 
-    $_SESSION[$div] = "do.php/$page";
-
-  load_js($div);
+  load_div($div, $page);
 }
 
 function set_div($div)
@@ -48,6 +47,7 @@ init_div('footer');
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
     <script type="text/javascript" src="../common/dom.js"></script> 
     <script type="text/javascript" src="../common/ajax.js"></script> 
     <script type="text/javascript">
