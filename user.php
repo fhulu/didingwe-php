1846566c44f831d44c4d6483d551d7c82a840540
fhulu <fhulu>
2012-01-30 17:25:06 +0000

    *** empty log message ***

---
+++ user.php
@@ -0,0 +1,19 @@
+<?php
+
+class user_exception extends Exception {};
+class user
+{
+  var $id;
+  var $partner_id;
+  var $role_id;
+  
+  var $email;
+  var $first_name;
+  var $last_name;
+  function __construct($data)
+  {
+    list($this->id, $this->partner_id, $this->email, $this->first_name, $this->last_name, $this->role_id) = $data;
+  }
+}
+
+?>
