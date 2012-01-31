20023e0f8273192fd9bbb8ee69054a19f419583d
linden <linden>
2012-01-31 12:49:24 +0000

    - getting program_id from config - using forgot_email for ajax email verification

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('db.php')
+require_once('db.php');
 require_once('config.php');
 
 class user_exception extends Exception {};
@@ -21,12 +21,11 @@ class user
   static function check($email=null)
   {
     if (is_null($email)) {
-      $email = $_GET[email];
+      $email = $_GET['forgot_email'];
       $ajax = true;
     }
-  
-    $sql = "select id from mukonin_audit.user where email_address='$email' and program_id = $session->program_id"; 
     
+    $sql = "select id from mukonin_audit.user where email_address='$email' and program_id = " . config::$program_id; 
     global $db;
     if ($db->exists($sql)) return true;
     
@@ -52,6 +51,7 @@ class user
   
   static function create($partner_id, $email, $first_name, $last_name, $role_id=0)
   {
+    $program_id = config::program_id;
     $sql = "insert into mukonin_audit.user(program_id, partner_id,first_name,last_name, role_id)
     values($program_id,$partner_id,'$first_name','$last_name',$role_id)";
     
@@ -59,7 +59,7 @@ class user
     $id = $db->insert($sql);
     if ($id == null) return false;
      
-    return new user(array($id, $program_id, $partner_id, $email, $first_name, $last_name, $role_id));
+    return new user(array($id, $partner_id, $email, $first_name, $last_name, $role_id));
   }
   
 }
