dde9f573457fcaf302f70da6085c4edbe69b148c
linden <linden>
2012-01-31 08:50:51 +0000

    - added debug info

--- user.php
+++ user.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('log.php');
+
 class user_exception extends Exception {};
 class user
 {
@@ -12,8 +14,10 @@ class user
   var $last_name;
   function __construct($data)
   {
-    list($this->id, $this->partner_id, $this->email, $this->first_name, $this->last_name, $this->role_id) = $data;
-  }
+  
+   list($this->id, $this->partner_id, $this->email, $this->first_name, $this->last_name, $this->role_id) = $data;
+    log::debug("User $this->email $this->first_name $this->last_name");
+   }
 }
 
 ?>
