8c97260ccdf87326aa2e60450a273f7bfba87b79
fhulu <fhulu>
2012-01-31 11:20:29 +0000

    - added authenitaction methods()

--- user.php
+++ user.php
@@ -14,10 +14,28 @@ class user
   var $last_name;
   function __construct($data)
   {
+    list($this->id, $this->partner_id, $this->email, $this->first_name, $this->last_name, $this->role_id) = $data;
+    log::debug("User $this->email $this->first_name $this->last_name");
+  }
+  
+  static function check($email, $ajax=true)
+  {
+    $sql = "select id from mukonin_audit.user where email_address='$email' and program_id = $session->program_id"; 
+    
+    global $db;
+    if ($db->exists($sql)) return true;
+    
+    if ($ajax)
+      echo "!We do not have a user with email address '$email' on our system";
+  }
+  
+  static function is_authentic($email, $password)
+  {
+    $sql = "select id, partner_id, email_address, first_name, last_name, role_id from mukonin_audit.user
+     where email_address='$email' and password=password('$passwd') and program_id = ". config::program_id;         
   
    list($this->id, $this->partner_id, $this->email, $this->first_name, $this->last_name, $this->role_id) = $data;
-    log::debug("User $this->email $this->first_name $this->last_name");
+
    }
 }
-
 ?>
