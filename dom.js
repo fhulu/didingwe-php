45ee1652c29533eda8fce3a7bd96e31f5411f966
fhulu <fhulu>
2012-02-27 07:28:58 +0000

    - added functions moved from sendSMS

--- dom.js
+++ dom.js
@@ -329,3 +329,56 @@ function SetValueOnSelect(dropdown, dest_id)
   });
 }
 
+function insertAtCursor(myField, myValue) {
+
+	//IE support
+
+	if (document.selection) {
+
+		myField.focus();
+
+		sel = document.selection.createRange();
+
+		sel.text = myValue;
+
+	}
+
+	//MOZILLA/NETSCAPE support
+
+	else if (myField.selectionStart || myField.selectionStart == '0') {
+
+		var startPos = myField.selectionStart;
+
+		var endPos = myField.selectionEnd;
+
+		myField.value = myField.value.substring(0, startPos)
+
+		+ '' + myValue + ' '
+
+		+ myField.value.substring(endPos, myField.value.length);
+    
+    + myField.focus();
+
+	} else {
+
+		myField.value += myValue;
+	}
+
+}
+    
+function preg_quote(str)
+{
+  return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
+
+function is_valid_msisdn(msisdn)
+{
+  var control = getElementByIdOrName(msisdn);
+  if (control.value.search(/^0[1-8][0-9]+$/) == -1) 
+  {
+    alert("Invalid Cell Number");
+    return false;
+  }
+  return true;
+}
+ 
