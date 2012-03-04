bd1dfdc4d16e2339bb317d00d25962723db6d830
fhulu <fhulu>
2012-03-04 16:48:01 +0000

    - using new queue management

--- iweb.php
+++ iweb.php
@@ -1,39 +1,42 @@
 <?php
 
-require_once('log.php');
-require_once('curl.php');
+require_once('../common/qworker.php');
+require_once('../common/log.php');
+require_once('../common/curl.php');
 
 class iweb_exception extends exception {};
 
-class iweb 
+class iweb extends qworker
 {
   var $session_id;
   var $curl;
-  var $path;
+  var $root;
   var $user_name;
   var $password;
+  var $db_read;
+  var $db_update;
 
-  function __construct($path, $user_name, $password, $port=80)
+  function __construct()
   {
+    parent::__construct();
     $this->curl = new curl();
-    $this->path = $path;
-    $this->user_name = $user_name;
-    $this->password = $password;
-    $this->port = $port;
-    $this->login();
+    $this->root = $this->provider_params['url'];
+    $this->user_name = $this->provider_params['user_name'];
+    $this->password = $this->provider_params['password'];
+    $this->port = $this->provider_params['port'];
   }
   
  
   function __destruct()
   {
-    $this->logout();
+    if ($this->is_connected()) $this->logout();
+    parent::__destruct();
   }
  
   function login()
   {
     if ($this->is_connected()) $this->logout();
-    
-    $url = "$this->path/Logon?UserId=$this->user_name&Password=$this->password";
+    $url = "$this->root/Logon?UserId=$this->user_name&Password=$this->password";
     log::info("Login $url");
     $result = $this->curl->read($url);
     log::debug("Login Response: $result");
@@ -43,22 +46,22 @@ class iweb
 Success&SessionID=b5749584541ff110fb72
 */
     $this->session_id = trim(substr($result,18));  
-    log::trace("Session ID: $this->session_id");
+    log::debug("Session ID: $this->session_id");
   }
   
   function is_connected() { return !is_null($this->session_id); }
   function logout()
   {
-    $url = "$this->path/Logoff?SessionID=$this->session_id";
-    $this->curl->read($url);
+    $url = "$this->root/Logoff?SessionID=$this->session_id";
     log::info("Logout $url");
+    $this->curl->read($url);
     $session_id = null;
   } 
 
-  function submit($msisdn, $reference, $message, $try_count=1, $encode=true)
+  function submit($id, $msisdn, $message, $try_count=1, $encode=true)
   {
     if ($encode) $message=urlencode($message);
-    $url = "$this->path/Submit?SessionID=$this->session_id&PhoneNumber=$msisdn&Reference=$reference&MessageText=$message";
+    $url = "$this->root/Submit?SessionID=$this->session_id&PhoneNumber=$msisdn&Reference=$id&MessageText=$message";
     $results = $this->curl->read($url);
     log::info("Submit $url");
     $results = explode('&', $results);
@@ -73,22 +76,51 @@ Success&SessionID=b5749584541ff110fb72
         $values[$value_pair[0]] = $value_pair[1];
     }
     if ($values[0] == 'Success') {
-      $values[0] = 1;
-      $values[1] = $values['MessageReference'];
+      ++$this->successes;
+      $status = 'sub';
+      $field = 'esme_reference';
+      $value = $values['MessageReference'];
     }
     else {
-      $values[0] = 0;
-      $error_code = $values['ErrorCode'];
+      ++$this->errors;
+      $value = $values['ErrorCode'];
       if ($try_count < 2) {
         $this->login();
-        return $this->submit($msisdn, $reference, $message, ++$try_count, false);
+        return $this->submit($id, $msisdn, $message, ++$try_count, false);
       }
       else {
-        $values[1] = $error_code;
+        $status = 'sub';
+        $field = 'error_code';
       }
     }
-    $values[2] = $try_count;
-    return $values;
+    $this->db_update->exec("update outq set status='$status',attempts=$attempts,$field='$value' where id = $id");
+  }
+ 
+  function start()
+  {
+    $this->login();
+    global $db;
+    $this->db_read = $db;
+    $this->db_update = $db->dup();
+    $self = &$this;
+    parent::listen(function($schedule_id, $qid) use (&$self)  {
+      if ($schedule_id == '' && $qid == '') {
+        log::error("No schedule_id or qid given for iweb");
+        return;
+      }
+      $sql = "select id,msisdn, message, reference1 from outq";
+      if ($id != '')
+        $sql .= " where id = $qid and status = 'pnd'";
+      else 
+        $sql .= " where schedule_id = $schedule_id and status = 'pnd' limit 0, $self->capacity";
+      $self->db_read->each($sql, function($index, $row) {
+        list($id, $msisdn, $message) = $row;
+        $self->submit($id, $msisdn, $message);
+      });
+    });
   }
 } 
+
+$iweb = new iweb();
+$iweb->start();
 ?>  
