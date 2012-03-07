6e173938aff94c9de112269e4e1c231f39b760f5
fhulu <fhulu>
2012-03-07 15:27:55 +0000

    - removed additional spaces

--- dom.js
+++ dom.js
@@ -329,41 +329,27 @@ function SetValueOnSelect(dropdown, dest_id)
   });
 }
 
-function insertAtCursor(myField, myValue) {
-
+function insertAtCursor(myField, myValue) 
+{
 	//IE support
-
 	if (document.selection) {
-
 		myField.focus();
-
 		sel = document.selection.createRange();
-
 		sel.text = myValue;
-
 	}
 
 	//MOZILLA/NETSCAPE support
-
 	else if (myField.selectionStart || myField.selectionStart == '0') {
-
 		var startPos = myField.selectionStart;
-
 		var endPos = myField.selectionEnd;
-
 		myField.value = myField.value.substring(0, startPos)
-
 		+ '' + myValue + ' '
-
 		+ myField.value.substring(endPos, myField.value.length);
-    
     + myField.focus();
-
-	} else {
-
+	} 
+  else {
 		myField.value += myValue;
 	}
-
 }
     
 function preg_quote(str)
