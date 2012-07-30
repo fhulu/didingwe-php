3c2bdb235e33dbb09db207e813b1c64718c9e4ec
fhulu <fhulu>
2012-07-30 12:30:02 +0000

    - left and right buttons aligned to edges

--- page_wizard.js
+++ page_wizard.js
@@ -15,20 +15,17 @@
         var page = $(this);
         page.hide();
         var bar = $("<div class='page-wizard-bar ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix'></div>");
-        var nav = $("<span class=page-wizard-nav></span>");
         var add_button = function(caption) {
           var name = caption.toLowerCase();
           var text = page.attr(name);
           if (text == undefined) return;
           if (text == '') text = caption;
-          var button = $("<button wizard="+name+">"+text+"</button>");
+          var button = $("<button wizard="+name+" class='page-wizard-button page-wizard-"+name+"'>"+text+"</button>");
           button.button();
-          nav.append(button);
+          bar.append(button);
         }        
         add_button('Back');
         add_button('Next');
-        
-        bar.append(nav);
         
         var display = page.attr('wizard');
         if (display === undefined || display == 'both' || display == 'bottom')
