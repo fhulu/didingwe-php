3dd7434f4e38bc60ed1e70985b6170e8e2647a7f
mampo <mampo>
2012-08-24 07:11:14 +0000

    -

--- index.php
+++ index.php
@@ -1,69 +1,105 @@
-<h1>Test Driving MySqlExcelBuilder</h1>
-<? 
-require_once('MySqlExcelBuilder.class.php');
-
-// Intialize the object with the database variables
-$database = 'xls_sample';
-$user='yourdbusername';
-$pwd='yourdbpassword';
-$mysql_xls = new MySqlExcelBuilder($database,$user,$pwd);
-
-// Setup the SQL Statements
-$sql_statement = <<<END_OF_SQL
-
-SELECT id,substring(vf.create_time,1,10) 'date',title,certificate_no,s.description status,
-                  case s.description
-                  when 'rejected' then 'Appeal'
-                  else '' 
-                  end as actions
-                  FROM film f, vendor_film vf,status s
-                  WHERE  f.id=vf.film_id and
-                  f.status=s.code
-                  and  vendor_id=324119
-
-END_OF_SQL;
-
-$sql_statement2 = <<<END_OF_SQL2
-
-select distinct title,g.id, publisher,max_age,g.consumer_advice,Concat( first_name, ' ', last_name ) AS contact_person,r.date_rated
-from game g,rating r, mukonin_audit.user u
-where g.id=r.game_id and r.user_id=u.id and g.user_id=u.id
-
-END_OF_SQL2;
+<?php
+session_start(); 
+require_once('log.php');
+log::init('index', log::DEBUG);
 
 
+function get_page_file($page)
+{
+  list($name, $params) = explode('?', $page);
+  static $extensions = array('','.php','.htm','.html');
+  foreach ($extensions as $ext) {
+    $file_name = $name . $ext;
+    if (file_exists($file_name))
+      return $file_name;
+  }
+  return null;
+}
 
-// Add the SQL statements to the spread sheet
-$mysql_xls->add_page('Film',$sql_statement,'Price','B',2);
-$mysql_xls->add_page('Game',$sql_statement2,'Price','B',2);
+function init_div($div, $default=null)
+{
+  $section = $div[0];
+  $page = $_GET[ $section ]; 
+  if ($page == '') {
+    $page = $_SESSION[$div];
+    if ($page != '') 
+      return;
+    $page = is_null($default)? $div: $default;
+  }
+  // determine file name from page
+  $file_name = get_page_file($page);
+  if (is_null($file_name)) {
+   
+    $params = '';
+    array_walk($_GET, function($value, $key) use (&$params, $section) {
+      if ($key != $section)
+        $params .= "&$key=".urlencode($value);
+    });
+    
+    $page .= $params;
 
-// Get the spreadsheet after the SQL statements are built...
-$phpExcel = $mysql_xls->getExcel(); // This needs to come after all the pages have been added.
+    $_SESSION[$div] = "/?a=$page";
+  }
+  else
+    $_SESSION[$div] = $file_name;
+  //echo "SESSION[div] = ". $_SESSION[$div] . "<br>\n";
+}
+function load_div($div)
+{
+  $page = $_SESSION[$div];
+  echo "<div id='$div'>";
+  if (strpos($page, '?') === false && file_exists($page)) {
+    require_once($page);
+  }
+  echo "</div>\n";
+}
 
-$phpExcel->setActiveSheetIndex(0); // Set the sheet to the first page.
-// Do some addtional formatting using PHPExcel
-$sheet = $phpExcel->getActiveSheet();
-$date = date('Y-m-d');
-$cellKey = "A1"; 
-$sheet->setCellValue($cellKey,"Gold Mugs Sold as Of $date");
-$style = $sheet->getStyle($cellKey);                              
-$style->getFont()->setBold(true);
 
-$phpExcel->setActiveSheetIndex(1); // Set the sheet to the second page.
-$sheet = $phpExcel->getActiveSheet(); 
-$sheet->setCellValue($cellKey,"Tea Sold as Of $date");
-$style = $sheet->getStyle($cellKey);                              
-$style->getFont()->setBold(true);
-
-$phpExcel->setActiveSheetIndex(0); // Set the sheet back to the first page.
-
-// Write the spreadsheet file...
-$objWriter = PHPExcel_IOFactory::createWriter($phpExcel, 'Excel5'); // 'Excel5' is the oldest format and can be read by old programs.
-$fname = "TestFile.xls";
-$objWriter->save($fname);
-
-// Make it available for download.
-echo "<a href=\"$fname\">Download $fname</a>";
-
+function set_div($div)
+{
+  $page = $_SESSION[$div];
+  if (strpos($page, '?') !== false) 
+    echo "ajax_inner('$div', '$page', true);\n";
+}
+if (isset($_GET['a'])) {
+  require_once('action.php');
+  return;
+}
+init_div('content', 'home');
+init_div('banner');
+init_div('menu');
+init_div('left-nav');
+init_div('right-nav');
+init_div('footer');
 
 ?>
+<!DOCTYPE html">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
+    <link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
+
+    <script type='text/javascript' src='jquery/min.js'></script>
+    <script type='text/javascript' src='jquery/ui-min.js'></script>
+    <script type='text/javascript' src='../common/dom.js'></script>
+    <script type="text/javascript" src="../common/ajax.js"></script> 
+ </head>
+  
+  <body>
+    <div id='frame'>
+      <div id='header'>
+        <?php load_div('banner'); ?>
+        <?php load_div('menu'); ?>
+      </div>
+      <div id='body'>
+        <?php load_div('left-nav'); ?>
+        <?php load_div('content'); ?>
+        <?php load_div('right-nav'); ?>
+      </div>
+      <?php load_div('footer'); ?>
+   </div>
+  </body>
+</html>
