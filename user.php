fac2cd6edecb7a3816cc0fd4eb109521084387c9
mampo <mampo>
2012-10-24 07:14:52 +0000

    --uncomment the email

--- user.php
+++ user.php
@@ -434,12 +434,12 @@ class user
   }
   
   static function verify_internal($request)
-  {/*
+  {
     $email = $request[email];
     if (config::$program_id == 3 && !preg_match('/@(fpb\.(org|gov)\.za|mukoni\.co\.za)$/i', $email)) {
       echo "!Application not yet released to the public. An announcement will be made soon.";
       return false;
-    }*/
+    }
     return true;
   }
   
