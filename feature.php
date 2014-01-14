32366feeb47d70a09bd6c79ef7a94de4e87d57af
mampo <mampo>
2014-01-14 08:47:33 +0000

    --checked if title is supplied

--- feature.php
+++ feature.php
@@ -19,9 +19,8 @@ class feature
   {
     
     $v = new validator($request); 
-    //$v->check('title')->is(2);
-    //$v->check('priority')->is('in(mukonin_audit.priority.code)'); 
-    $v->check('reason')->is(30);
+    $v->check('title')->is(2); 
+    $v->check('description')->is(30);
     return $v->valid();
   }
 
@@ -30,23 +29,47 @@ class feature
     return select::add_db('select code, name from mukonin_audit.priority order by no','','','--Select Priority--');
   }
  
+  static function status()
+  {
+    return select::add_db('select code, description from mukonin_audit.status where code !="decl"','','','--Select Status--');
+  }
   static function save($request)
   {    
     global $db,$session;
     
     $user_id= $session->user->id;
     $partner_id= $session->user->partner_id;
-    $title = $request[title];
-    $type = $request[type];
-    $reason = $request[reason];  
+    $title = $request[title];;
+    $description = $request[description];  
     $program_id = config::$program_id;
-  
-    //TO DO create a function in DB
-     //user::audit('update_role', $id, $role);
-    
+      
     $sql = "INSERT into mukonin_audit.feature_request(program_id,user_id,partner_id,title,description,priority,status_code)
-            VALUES($program_id,$user_id,$partner_id,'$title','$reason','med','pend')";
+            VALUES($program_id,$user_id,$partner_id,'$title','$description','med','pend')";
     $db->exec($sql);
+    
+    list($email_address,$requestor)= $db->read_one("SELECT email_address,concat(first_name,' ' ,last_name) user from mukonin_audit.user  
+                                   where id=$user_id");
+    $rows = $db->read("select email_address from mukonin_audit.user u, mukonin_audit.partner p, mukonin_audit.user_role r
+    where u.partner_id = p.id and r.user_id = u.id and r.role_code='admin' and short_name='FPB'");
+
+   
+    foreach($rows as $row)
+    { 
+        $username = $row['contact_person'];
+        $email = $row['email_address'];
+
+        $message = "Good day<br><br>
+        I would like to have the following feature added to the system.<br><br>
+        $description.<br><br>
+        Regards,<br>
+        $requestor";
+        $subject = "Feature Request, ".$title;
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+        $headers .= "from:  $email_address";
+        $mail_sent = mail($email, $subject, $message, $headers);
+        log::debug("Sending email $email_address to $username<$email>: $mail_sent");    
+     }
       
   }
  
@@ -56,11 +79,12 @@ class feature
     $user_id = $session->user->id;
     $id = $request['id'];
     $priority = $request['priority'];
+    $status = $request['status'];
     //user::audit('update_role', $id, $priority);
     
     global $db;
    
-    $sql = "update mukonin_audit.feature_request set priority='$priority' where id = $id";
+    $sql = "update mukonin_audit.feature_request set priority='$priority',status_code='$status' where id = $id";
     $db->exec($sql);
        
    }
@@ -69,34 +93,98 @@ class feature
   static function manage_features($request)
   {
     //user::verify('manage_features');
-
-    global $session;
-    $user = $session->user;
-    $program_id = config::$program_id;
-    $partner_id = $request['partner_id'];
-    if ($partner_id == '') $partner_id = $user->partner_id;
-    $titles = array('#id','Time','~Company','~Requestor','~Request','~Reason','~Priority|name=priority|edit=list:?feature/priorities','~Status','~Status Reason','Action');
+   $program_id = config::$program_id;
+    $titles = array('#id','Time','~Company','~Requestor','~Request','~Reason','~Priority|name=priority|edit=list:?feature/priorities','~Status|name=status|edit=list:?feature/status','~Status Reason','Action');
     $table = new table($titles, table::TITLES | table::ALTROWS | table::FILTERABLE | table::EXPORTABLE);
     $table->set_heading("Manage Feature Request");
     $table->set_options($request);$table->set_key('id');
-    $table->set_row_actions('edit,Approve,Reject');
+    
+    $table->set_expandable('/?a=request_comment');
+    $table->set_row_actions('edit,Decline,Comment');
     $table->set_saver('/?a=feature/update_priority');
     $table->set_options($request);
-    $sql = "select * from (select fr.id,fr.create_time,full_name,concat(first_name,' ',last_name) requestor,fr.title,fr.description,pr.name priority,s.description status, r.description reason,
-       case s.description
-              when 'pending' then 'edit,Approve,Reject'
-              when 'approved' then 'edit,Reject'
-              when 'rejected' then 'edit,Approve'
-              else '' 
-            end as actions
+    $sql = "select * from (select fr.id,fr.create_time,full_name,concat(first_name,' ',last_name) requestor,fr.title,fr.description,pr.name priority,s.description status, r.description reason
             from mukonin_audit.feature_request fr
             join mukonin_audit.user u on u.id = fr.user_id
             join mukonin_audit.partner p on p.id = fr.partner_id
             join mukonin_audit.status s on s.code = fr.status_code
             left join mukonin_audit.priority pr on pr.code = fr.priority
-            left join mukonin_audit.reason r on r.code = fr.status_reason_code) tmp where 1=1";
+            left join mukonin_audit.reason r on r.code = fr.status_reason_code 
+            where fr.program_id=$program_id) tmp where 1=1";
     $table->show($sql);
   }
+  static function detail($request)
+  {      
+    $id = $request['id'];
+    //user::verify('process_licence', $id);  
+    $sql = "select  title,description reason,count(comment) comments
+            from mukonin_audit.feature_request fr
+            left join mukonin_audit.feature_comment fc on fr.id=fc.feature_id
+            where fr.id = $id ";
+     
+    global $db;
+    $result = $db->read_one($sql, MYSQLI_ASSOC);
+    
+    echo json_encode($result);
+  }
+  
+    static function answers($request)
+  {      
+    $id = $request['id']; 
+    $program_id = config::$program_id;
+    
+    $sql = "SELECT distinct fc.id,fc.create_time date,comment,concat(first_name,' ',last_name)
+            from mukonin_audit.feature_comment fc 
+            join mukonin_audit.user u on u.id = fc.user_id
+            WHERE feature_id= $id and fc.program_id=$program_id";
+     
+    $headings = array('#id|name=id','Date', 'Comments','User','#hi');
+    $table = new table($headings,table::TITLES | table::ALTROWS);
+    unset($request['id']);
+    $table->set_options($request);
+    $table->show($sql);
+  }
+  static function view_feature($request)
+  {
+    //user::verify('manage_feedbacks');
+    $program_id = config::$program_id;
+    $titles = array('#id','Time','~Requestor','~Request ','~Status','~Action');
+    $table = new table($titles, table::TITLES | table::ALTROWS | table::FILTERABLE | table::EXPORTABLE);
+    $table->set_heading("View Feature Requests");
+    $table->set_options($request);$table->set_key('id');
+    $table->set_row_actions('Comment');
+    $table->set_expandable('/?a=request_comment');
+    $table->set_options($request);
+    $sql = "select * from (select fr.id,fr.create_time,concat(first_name,' ',last_name) requestor,fr.title,s.description,
+            case s.description
+             when 'pending' then 'Comment'
+              when 'planned' then 'Comment'
+              when 'declined' then ''
+              when 'under review' then 'Comment'
+              else '' 
+            end as actions
+            from mukonin_audit.feature_request fr
+            join mukonin_audit.user u on u.id = fr.user_id
+            join mukonin_audit.status s on s.code = fr.status_code
+            join mukonin_audit.partner p on p.id = fr.partner_id
+           where fr.program_id=$program_id) tmp where 1=1";
+    $table->show($sql);
+  }
+  
+  
+  static function add_comment($request)
+  {
+    global $session,$db;  
+    
+    $user_id = $session->user->id;
+    $program_id = config::$program_id;
+    $id= $request[id];
+    $comment= $request[comments];
+    
+    $sql = "INSERT into mukonin_audit.feature_comment(program_id,feature_id,user_id,comment)
+            VALUES($program_id,$id,$user_id,'$comment')";
+    $db->exec($sql);
+    }
   static function approve($request)
   {
     global $db;   
@@ -160,13 +248,13 @@ class feature
     $status_code = substr(strtolower($request['code']),0,4);
     $reason_code = $request['reason'];
     $title = $db->read_one_value("select title from mukonin_audit.feature_request where id = $id");
-    $status = $db->read_one_value("select description from mukonin_fpb.status where code = '$status_code'"); 
+    $status = $db->read_one_value("select description from mukonin_audit.status where code = '$status_code'"); 
     $reason = $db->read_one_value("select description from mukonin_fpb.reason where code = '$reason_code'"); 
-    user::audit($status_code."_feature_request",$id,"Title: $title, Reason: $reason");
+    user::audit("reje_feature_request",$id,"Title: $title, Status:$status,Reason: $reason");
     
     $sql = "update mukonin_audit.feature_request
-              set status_code='$status_code',
-             status_reason_code='$reason_code'
+            set status_code='$status_code',
+            status_reason_code='$reason_code'
             where id = $id";
     
     $db->exec($sql);  
