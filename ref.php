640bb04ab73066765b1dbdece4f001ff25152684
fhulu <fhulu>
2014-01-28 13:13:05 +0000

    - code to cater for columns list1 and list2 removed from ref_assoc - added add(), assoc() and delete()

--- ref.php
+++ ref.php
@@ -18,7 +18,8 @@ class ref
   {
     $list = addslashes($request['list']);    
     $sql = "select item_code, item_name, item_desc from mukonin_audit.ref_list "
-            . "where list_name = '$list'";
+            . "where list_name = '$list'"
+            . " and program_id = \$pid";
     return ref::encode_sql($request, $sql);
   }
   
@@ -84,17 +85,82 @@ class ref
     $list = addslashes($request['list']);
     $code = addslashes($request['code']);
     
-    $sql = "select a.$result_code, l.item_name, l.item_desc "
-            . "from mukonin_audit.ref_assoc a "
-            . "join mukonin_audit.ref_list l on l.item_code = a.$result_code "
-            . " and a.name = '$name' and a.$result_list = '$list'"
-            . " and a.$search_code = '$code'";
+    $sql = "select result.item_code, result.item_name, result.item_desc "
+            . " from mukonin_audit.ref_assoc assoc "
+            . " join mukonin_audit.ref_list vars on "
+            .   " assoc.program_id = vars.program_id "
+            .   " and assoc.name = vars.list_name "
+            .   " and assoc.program_id = \$pid"
+            .   " and assoc.name = '$name'"
+            .   " and assoc.$search_code = '$code' "
+            .   " and vars.item_code = '$result_list' "             
+            . " join mukonin_audit.ref_list result on "
+            .    " assoc.program_id = result.program_id "
+            .    " and vars.item_name = result.list_name "
+            .    " and result.item_code = assoc.$result_code";
     
     return ref::encode_sql($request, $sql);
   }
 
   static function reverse_query($request)
   {
-    return ref::assoc($request, 'code2', 'list1', 'code1');
+    return ref::query($request, 'code2', 'list1', 'code1');
+  }
+  
+  static function add($request)
+  {
+    $v = new validator($request);
+    $v->check('list')->is('at_least(2)');
+    $v->check('name')->is('at_least(2)');
+    $v->check('code')->is('at_least(2)');
+    if (!$v->valid()) return false;
+
+    $list = addslashes($request['list']);
+    $code = addslashes($request['code']);
+    $name = addslashes($request['name']);
+    $desc = addslashes($request['desc']);
+
+    $sql = "insert into mukonin_audit.ref_list(program_id, list_name, item_code, item_name, item_desc)"
+            . "values (\$pid, '$list', '$code', '$name', '$desc')";
+    
+    global $db;
+    $db->exec($sql);
+  }
+
+  static function delete($request)
+  {
+    $list = addslashes($request['list']);
+    $code = addslashes($request['code']);
+
+    $sql = "delete from mukonin_audit.ref_list"
+            . "where program_id = \$pid and  list_name = '$list', item_code='$code'";
+   
+    global $db;
+    $db->exec($sql);
+  }
+  
+  static function assoc($request)
+  {
+    $program_id = config::$program_id;
+    $v = new validator($request);
+    $v->check('name')->is('at_least(2)');
+    $v->check('list1')->is('at_least(2)');
+    $v->check('code1')->is('at_least(2)');
+    $v->check('list2')->is('at_least(2)');
+    $v->check('code2')->is('at_least(2)');
+    if (!$v->valid()) return false;
+    
+    $name = addslashes($request['name']);
+    $desc = addslashes($request['list1_desc']);
+    $code = addslashes($request['code1']);
+
+    global $db;
+    $db->exec("insert ignore mukonin_audit.ref_list(program_id, list_name, item_code, item_name, item_desc)"
+            . "values(\$pid, '$name', '$list', '$code', '$desc')");
+    
+    $desc = addslashes($request['list2_desc']);
+    $code = addslashes($request['code2']);
+    $db->exec("insert ignore mukonin_audit.ref_list(program_id, list_name, item_code, item_name, item_desc)"
+            . "values(\$pid, '$name', '$list', '$code', '$desc')");
   }
 }
