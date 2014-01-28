5640632631dac9dbd26b6a8e2fceb2baefacf1bf
fhulu <fhulu>
2014-01-28 09:14:10 +0000

    - refactored assoc() and reverse_assoc() - renamed assoc() and reverse_assoc() to query() and reverse_query() - refactored encoder methods encode_json() and and encode_select()

--- ref.php
+++ ref.php
@@ -39,63 +39,62 @@ class ref
     return $sql;
   }
   
-  static function encode_sql($request, $sql)
+  static function encode_json($request, $sql)
   {
     global $db;
-    
-    $sql .= ref::get_sql_extension($request);
-    list($encode) = explode('/', $_GET['a']);
-    if ($encode == 'json') {
-      $rows = $db->read($sql, MYSQL_ASSOC);
-      echo json_encode ($rows);
-      return;
-    }
-    if ($encode == 'select') {
-      $list = ucwords(str_replace('_', ' ', $request['list']));
-      $selected = $request['selected'];
-      if ($selected == '')
-        echo "<option selected>--Select $list--</option>\n";
-      else 
-        echo "<option selected>--Select $list--</option>\n";
-      $rows = $db->read($sql, MYSQL_NUM);
-      foreach($rows as $row) {
-        $key = $row[0];
-        $item =  $row[1];
-        if ($selected == $key) $selected = " selected";
-        echo "<option value='$key'$selected>$item</option>\n";
-      }
-      return;
-    }
-    log::warn("Encoding type not specified");
+    $rows = $db->read($sql, MYSQL_ASSOC);
+    echo json_encode ($rows);
   }
   
-  static function assoc($request)
+  static function encode_select($request, $sql)
+  {
+    $list = ucwords(str_replace('_', ' ', $request['list']));
+    $selected = $request['selected'];
+    if ($selected == '')
+      echo "<option selected>--Select $list--</option>\n";
+    else 
+      echo "<option selected>--Select $list--</option>\n";
+    
+    global $db;
+    $rows = $db->read($sql, MYSQL_NUM);
+    foreach($rows as $row) {
+      $key = $row[0];
+      $item =  $row[1];
+      if ($selected == $key) $selected = " selected";
+      echo "<option value='$key'$selected>$item</option>\n";
+    }    
+  }
+
+  static function encode_sql($request, $sql)
+  {
+    $sql .= ref::get_sql_extension($request);
+    list($encode) = explode('/', $_GET['a']);
+    $callback = "ref::encode_$encode";
+    if (!is_callable($callback)) {
+      log::warn("Encoding type not specified");
+      return;
+    }
+    
+    return call_user_func($callback, $request, $sql);
+  }
+  
+  static function query($request, $search_code='code1', $result_list='list2', $result_code='code2')
   {
     $name = addslashes($request['name']);
     $list = addslashes($request['list']);
     $code = addslashes($request['code']);
     
-    $sql = "select a.code2, l.item_name, l.item_desc "
+    $sql = "select a.$result_code, l.item_name, l.item_desc "
             . "from mukonin_audit.ref_assoc a "
-            . "join mukonin_audit.ref_list l on l.item_code = a.code2 "
-            . " and a.name = '$name' and a.list2 = '$list'"
-            . " and a.code1 = '$code'";
+            . "join mukonin_audit.ref_list l on l.item_code = a.$result_code "
+            . " and a.name = '$name' and a.$result_list = '$list'"
+            . " and a.$search_code = '$code'";
     
     return ref::encode_sql($request, $sql);
   }
 
-  static function reverse_assoc($request)
+  static function reverse_query($request)
   {
-    $name = addslashes($request['name']);
-    $list = addslashes($request['list']);
-    $code = addslashes($request['code']);
-    
-    $sql = "select a.code1, l.item_name, l.item_desc "
-            . "from mukonin_audit.ref_assoc a "
-            . "join mukonin_audit.ref_list l on l.item_code = a.code1 "
-            . " and a.name = '$name' and a.list1 = '$list'"
-            . " and a.code2 = '$code'";
-    
-    return ref::encode_sql($request, $sql);
+    return ref::assoc($request, 'code2', 'list1', 'code1');
   }
 }
