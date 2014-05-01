507e81d20dcd2e278d1948340e3c73f77931058b
fhulu <fhulu>
2014-05-01 17:15:09 +0000

    - now handles form wizard

--- form.php
+++ form.php
@@ -15,18 +15,31 @@
 require_once 'db.php';
 class form {
 
-  static function get_fields($sql)
+  static function get_fields($sql, $key='code')
   {
     global $db;
     $rows = $db->read($sql,MYSQLI_ASSOC);
    
     $fields = array();
     foreach($rows as $row) {
-      $code = $row['code'];
-      unset($row['code']);
+      $code = $row[$key];
+      unset($row[$key]);
       $fields[$code] = $row;
     }
     return $fields;
+  }
+  
+  static function read_attributes($code)
+  {
+    global $db;
+    $program_name = config::$program_name;
+    return $db->read_one("select '$program_name' program, function_code, title, method, class"
+            . ", replace(description, '\$program', '$program_name') 'desc'"
+            . ", class, fields_class, label_position, data_url"
+            . " from mukonin_form.form "
+            . " where program_id in ('\$pid', '_generic') and code = '$code'"
+            . " order by program_id desc", MYSQLI_ASSOC);
+    
   }
   static function load($request) 
   {
@@ -34,15 +47,8 @@ class form {
     $code = $request['code'];
     $program_name = config::$program_name;
     
-    // read form attributes
-    $attributes = $db->read_one("select '$program_name' program, function_code, title, method"
-            . ", replace(description, '\$program', '$program_name') 'desc'"
-            . ", class, fields_class, label_position, data_url"
-            . " from mukonin_form.form "
-            . " where program_id in ('\$pid', '_generic') and code = '$code'"
-            . " order by program_id desc", MYSQLI_ASSOC);
+    $attributes = form::read_attributes($code);
     
-    // read fields
     $fields = form::get_fields("select distinct f.code"
             . ", ifnull(ff.my_name, f.name) name, f.length, ff.parent_field parent, ff.visible, ff.optional"
             . ", replace(ifnull(ff.my_description, f.description), '\$program', '$program_name') 'desc'"
@@ -53,7 +59,7 @@ class form {
             . " and f.code = ff.field_code and ff.form_code = '$code'"
             . " order by ff.program_id desc, ff.position asc");
     
-    $actions = form::get_fields("select distinct f.code"
+    $actions = form::get_fields("select distinct f.code, fa.method"
             . ",  ifnull(fa.my_name,f.name) name, fa.visible"
             . ", replace(ifnull(fa.my_description, f.description), '\$program', '$program_name') 'desc'"
             . ", ifnull(fa.my_input,f.input) input"
@@ -66,6 +72,21 @@ class form {
         'attributes'=>$attributes,
         'fields'=>$fields,
         'actions'=>$actions));
+  }
+  
+  static function load_wizard($request)
+  {
+    $code = $request['code'];
+    $forms = form::get_fields("select f.code, f.title, wf.nav_position, wf.show_back, wf.show_next, wf.next_action"
+            . " from mukonin_form.wizard_form wf join mukonin_form.form f "
+            . " on f.program_id = wf.program_id and f.program_id in ('\$pid', '_generic')"
+            . " and f.code = wf.form_code"
+            . " and wf.wizard_code = '$code'"
+            . " order by wf.program_id, wf.position asc");
+    echo json_encode(array(
+        "program"=>config::$program_name,
+        "size"=>sizeof($forms), 
+        "forms"=>$forms));
   }
   
   static function test()
