c0845efc2fe24d0b4e36203517d0b1403ebc9eb0
fhulu <fhulu>
2014-05-01 02:47:57 +0000

    - allow base fields to be overridden

--- form.php
+++ form.php
@@ -37,33 +37,27 @@ class form {
     // read form attributes
     $attributes = $db->read_one("select '$program_name' program, function_code, title, method"
             . ", replace(description, '\$program', '$program_name') 'desc'"
-            . ", class, fields_class, label_position"
+            . ", class, fields_class, label_position, data_url"
             . " from mukonin_form.form "
             . " where program_id in ('\$pid', '_generic') and code = '$code'"
             . " order by program_id desc", MYSQLI_ASSOC);
     
     // read fields
-    $rows = $db->read("select f.code, f.name, f.length, ff.filter_field filter, ff.visible, ff.optional"
-            . ", replace(f.description, '\$program', '$program_name') 'desc'"
-            . ", ifnull(ff.input,f.input) input"
-            . ", ifnull(ff.reference, f.reference) reference"
-            . " from mukonin_form.field f join mukonin_form.form_field ff"
-            . " on f.program_id = ff.program_id and f.program_id in ('\$pid', '_generic')"
-            . " and f.code = ff.field_code and ff.form_code = '$code'",MYSQLI_ASSOC);
-   
-    $fields = form::get_fields("select distinct f.code, f.name, f.length, ff.filter_field filter, ff.visible, ff.optional"
-            . ", replace(f.description, '\$program', '$program_name') 'desc'"
-            . ", ifnull(ff.input,f.input) input"
-            . ", ifnull(ff.reference, f.reference) reference"
+    $fields = form::get_fields("select distinct f.code"
+            . ", ifnull(ff.my_name, f.name) name, f.length, ff.parent_field parent, ff.visible, ff.optional"
+            . ", replace(ifnull(ff.my_description, f.description), '\$program', '$program_name') 'desc'"
+            . ", ifnull(ff.my_input,f.input) input"
+            . ", ifnull(ff.my_reference, f.reference) reference"
             . " from mukonin_form.field f join mukonin_form.form_field ff"
             . " on f.program_id = ff.program_id and f.program_id in ('\$pid', '_generic')"
             . " and f.code = ff.field_code and ff.form_code = '$code'"
             . " order by ff.program_id desc, ff.position asc");
     
-    $actions = form::get_fields("select distinct f.code, f.name, fa.visible"
-            . ", replace(f.description, '\$program', '$program_name') 'desc'"
-            . ", ifnull(fa.input,f.input) input"
-            . ", ifnull(fa.reference, f.reference) reference"
+    $actions = form::get_fields("select distinct f.code"
+            . ",  ifnull(fa.my_name,f.name) name, fa.visible"
+            . ", replace(ifnull(fa.my_description, f.description), '\$program', '$program_name') 'desc'"
+            . ", ifnull(fa.my_input,f.input) input"
+            . ", ifnull(fa.my_reference, f.reference) reference"
             . " from mukonin_form.field f join mukonin_form.form_action fa"
             . " on f.program_id = fa.program_id and f.program_id in ('\$pid', '_generic')"
             . " and f.code = fa.field_code and fa.form_code = '$code'"
@@ -72,6 +66,11 @@ class form {
         'attributes'=>$attributes,
         'fields'=>$fields,
         'actions'=>$actions));
+  }
+  
+  static function test()
+  {
+    echo json_encode(array("za_city"=>"7100"));
   }
   //put your code here
 }
