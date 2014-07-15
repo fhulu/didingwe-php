3736d8e0f24ffa65e5da247484b6ad6b8dd9023d
fhulu <fhulu>
2014-07-15 08:03:16 +0000

    - using field table, no form or wizard

--- page.php
+++ page.php
@@ -16,88 +16,62 @@ require_once 'db.php';
 require_once 'validator.php';
 require_once 'ref.php';
 
-class form {
-
-  static function get_fields($sql, $key='code')
+class page {
+  
+  static function expand(&$row, $field)
   {
-    global $db;
-    $rows = $db->read($sql,MYSQLI_ASSOC);
-   
-    $fields = array();
-    foreach($rows as $row) {
-      $code = $row[$key];
-      unset($row[$key]);
-      $fields[$code] = $row;
+    $extras = explode('~',$row[$field]);
+    foreach($extras as $attr) {
+      $attr = explode('=>',$attr);
+      $row[$attr[0]] = $attr[1];
     }
-    return $fields;
+    unset($row[$field]);
+    $html = &$row['html'];
+    foreach($row as $key=>&$value) {
+      $value = str_replace('$program', config::$program_name, $value);
+      $html = str_replace("\$$key", $value, $html);
+    }
   }
   
-  static function read_attributes($code)
+  static function read_children($code)
   {
     global $db;
-    $program_name = config::$program_name;
-    return $db->read_one("select '$program_name' program, function_code, title, method, class"
-            . ", replace(description, '\$program', '$program_name') 'desc'"
-            . ", class, fields_class, label_suffix, label_position, data_url"
-            . " from form "
-            . " where program_id in ('\$pid', '_generic') and code = '$code'"
-            . " order by program_id desc", MYSQLI_ASSOC);
-    
+    $rows =  $db->read("select f.code, f.type, cf.enabled, cf.visible, ft.html,
+                cf.optional,f.name, f.initial_value value, f.description 'desc',
+                f.custom, cf.custom overridden_custom
+                from container_field cf join field f on cf.field_code = f.code 
+                join field_type ft on f.type = ft.code
+                where cf.parent_field_code = '$code'
+                order by cf.position asc", MYSQLI_ASSOC);
+    $children = array();
+    foreach($rows as &$row) {
+      $code = $row['code'];
+      page::expand($row, 'overridden_custom');
+      page::expand($row, 'custom');
+      unset($row['code']);
+      if (in_array($row['type'], array('page','container','form'))) 
+        $row['children'] = page::read_children($code);
+      $children[$code] = $row;
+    }
+    return $children;
   }
-  static function load($request) 
+  
+  
+  static function read($request)
   {
     global $db;
     $code = $request['code'];
-    $program_name = config::$program_name;
 
-    $forms = form::get_fields("select f.code, f.title, wf.nav_position, wf.show_back, wf.show_next, wf.next_action"
-            . " from wizard_form wf join form f "
-            . " on f.program_id in ('\$pid', '_generic')"
-            . " and wf.program_id in ('\$pid', '_generic')"
-            . " and f.code = wf.form_code"
-            . " and wf.wizard_code = '$code'"
-            . " order by wf.program_id desc, wf.position asc");
-    if (sizeof($forms) != 0) {
-      echo json_encode(array(
-          "program"=>config::$program_name,
-          "size"=>sizeof($forms), 
-          "forms"=>$forms));
-      return;
-    }
+    global $db;
+    $row =  $db->read_one("select f.code,type, f.name, f.description 'desc', initial_value value, html, custom"
+            . " from field f join field_type ft on f.type = ft.code"
+            . " where f.code = '$code'", MYSQLI_ASSOC);
     
-    $attributes = form::read_attributes($code);
+    page::expand($row, 'custom');
+    if (in_array($row['type'], array('page','container','form','input_page'))) 
+      $row['children'] = page::read_children($code);
     
-    $fields = form::get_fields("select distinct ff.enabled"
-            . ", ifnull(ff.my_field_code, f.code) code"
-            . ", ifnull(ff.my_name, f.name) name"
-            . ", ifnull(ff.my_initial_value, f.initial_value) initial"
-            . ", ifnull(ff.my_size, f.size) size"
-            . ", ff.parent_field parent, ff.visible, ff.optional"
-            . ", replace(ifnull(ff.my_description, f.description), '\$program', '$program_name') 'desc'"
-            . ", ifnull(ff.my_input,f.input) input"
-            . ", ifnull(ff.my_reference, f.reference) reference"
-            . " from form_field ff left join field f"
-            . " on f.program_id in ('\$pid', '_generic')"
-            . " and f.code = ff.field_code"
-            . " where ff.program_id in ('\$pid', '_generic')"
-            . " and ff.form_code = '$code'"
-            . " order by ff.program_id desc, ff.position asc");
-    
-    $actions = form::get_fields("select distinct fa.field_code code, fa.method, fa.validator"
-            . ", ifnull(fa.my_name,f.name) name, fa.visible"
-            . ", replace(ifnull(fa.my_description, f.description), '\$program', '$program_name') 'desc'"
-            . ", ifnull(fa.my_input,f.input) input"
-            . ", ifnull(fa.my_reference, f.reference) reference"
-            . " from form_action fa left join field f"
-            . " on f.program_id in ('\$pid', '_generic')"
-            . " and f.code = fa.field_code"
-            . " where fa.program_id in ('\$pid', '_generic')"
-            . " and fa.form_code = '$code'"
-            . " order by fa.program_id desc, fa.position asc");
-    echo json_encode(array(
-        'attributes'=>$attributes,
-        'fields'=>$fields,
-        'actions'=>$actions));
+    echo json_encode($row);
   }
   
   static function validate($request)
