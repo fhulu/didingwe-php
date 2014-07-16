e3f028624678d734a1615f2e968dd792402f401c
fhulu <fhulu>
2014-07-16 18:02:11 +0000

    - derived from show_form.php

---
+++ show_page.php
@@ -0,0 +1,30 @@
+<?php
+require_once ('log.php');
+$page = $_REQUEST['page'];
+log::debug("loading $page");
+?>
+    <link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
+    <link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
+
+    <script type='text/javascript' src='jquery/min.js'></script>
+    <script type='text/javascript' src='jquery/ui-min.js'></script>
+    <script type="text/javascript" src='common/mukoni.jquery.js'></script> 
+    <script type="text/javascript" src='common/mukoni.jquery-ui.js'></script> 
+<link type="text/css" rel="stylesheet" href="page_wizard.css"></link>
+<?php
+if (file_exists("$page.css")) { 
+  echo "<link type='text/css' rel='stylesheet' href='$page.css'></link>";
+} 
+?>
+<script type='text/javascript' src="common/page.js"></script>
+<script type='text/javascript' src="common/page_wizard.js"></script>
+<script>
+$(function() {
+  $("#<?=$page; ?>").page();
+});
+</script>
+<?php 
+if (file_exists("$page.js")) { 
+  echo "<script type='text/javascript' src='$page.js'></script>";
+}
+echo "<div id='$page'></div>";
