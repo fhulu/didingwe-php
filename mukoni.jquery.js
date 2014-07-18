7e6ab567f0ff6f9cd09c204c7cb5444955f400c4
fhulu <fhulu>
2014-07-18 13:28:59 +0000

    - added sleep()

--- mukoni.jquery.js
+++ mukoni.jquery.js
@@ -530,3 +530,15 @@ function delegate(scope, func, data, isTimeout)
         func.apply(scope, args);
     }
 }
+
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
