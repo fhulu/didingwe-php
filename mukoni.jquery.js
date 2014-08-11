25e1d05033e46add40d919e85b53aa50a59f628c
fhulu <fhulu>
2014-08-11 10:08:26 +0000

    - added getQueryParams

--- mukoni.jquery.js
+++ mukoni.jquery.js
@@ -547,3 +547,17 @@ function sleep(milliseconds) {
   }
 }
 
+
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
