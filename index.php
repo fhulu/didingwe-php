9f7a32693dcf80cc6cedac7fd918104d80e018ca
fhulu <fhulu>
2014-09-03 06:13:22 +0000

    - now using page.js - no longer using any template

--- index.php
+++ index.php
@@ -1,179 +1,66 @@
 <?php
-session_start(); 
-require_once('../common/log.php');
-log::init('index', log::DEBUG);
-log::debug($_SERVER['QUERY_STRING']);
 if (isset($_GET['a'])) {
   require_once('action.php');
   return;
 }
-
-
-function get_page_file($page)
-{
-  list($name, $params) = explode('?', $page);
-  static $extensions = array('','.htm','.html','.php');
-  foreach ($extensions as $ext) {
-    $file_name = $name . $ext;
-    if (file_exists($file_name) && filetype($file_name) != 'dir')
-      return $file_name;
-    $file_name = '../common/'.$file_name;
-    if (file_exists($file_name) && filetype($file_name) != 'dir')
-      return $file_name;
-  }
-  return $name;
-}
-
-function init_div($div, $default=null)
-{
-  $section = $div[0];
-  $page = $_GET[ $section ]; 
-  if ($page == '') {
-    $page = $_SESSION[$div];
-    if ($page != '') 
-      return;
-    $page = is_null($default)? $div: $default;
-  }
-  // determine file name from page
-  $file_name = get_page_file($page);
-  if (is_null($file_name)) {
-   
-    $params = '';
-    array_walk($_GET, function($value, $key) use (&$params, $section) {
-      if ($key != $section)
-        $params .= "&$key=".urlencode($value);
-    });
-    
-    $page .= $params;
-
-    $_SESSION[$div] = "?a=$page";
-  }
-  else
-    $_SESSION[$div] = $file_name;
-  //echo "SESSION[div] = ". $_SESSION[$div] . "<br>\n";
-}
-function load_div($div)
-{
-  $page = $_SESSION[$div];
-  $common_page = "../common/$page";
-  echo "<div id='$div'>";
-  if (strpos($page, '?') === false) {
-    try {
-      if (file_exists($page))
-        require_once($page);
-      else if (file_exists($common_page))
-        require_once($common_page);
-      else {
-        $_GET['_code'] = $page;
-        include('../common/show_form.php');
-      }
-    }
-    catch (user_exception $exception) {
-      log::error("UNCAUGHT EXCEPTION: " . $exception->getMessage() );
-      echo "<script>location.href='login.html';</script>";
-    }
-    catch (Exception $exception)
-    {
-      $message = $exception->getMessage();
-      log::error("UNCAUGHT EXCEPTION: $exception");
-      $message = "EXCEPTION on ". config::$program_name. ": $message";
-      user::sms('0828992177', 2, 18, $message);
-      user::sms('0836570252', 2, 18, $message);
-      user::sms('0731854690', 2, 18, $message);
-      user::sms('0743272009', 2, 18, $message);
-      require_once('error.php');
-    }
-  }
-  echo "</div>\n";
-}
-
-
-function set_div($div)
-{
-  $page = $_SESSION[$div];
-  if (strpos($page, '?') !== false) 
-    echo "ajax_inner('$div', '$page', true);\n";
-}
-header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
-header('Pragma: no-cache'); // HTTP 1.0.
-header('Expires: 0'); // Proxies.
-init_div('content', 'home');
-init_div('banner');
-init_div('menu');
-init_div('left-nav');
-init_div('right-nav');
-init_div('footer');
 ?>
-<!DOCTYPE html>
 
-<html >
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--[if IE]>
-    <style>
-    .labels>*
-    {
-      padding-top: 3px;
-      padding-top: 3px;
-    }
-    .controls>select
-    {
-      height: 26px;
-    }
-    </style>
-    <![EndIf]-->
-<!--
-<link rel ="stylesheet" href ="css/bootstrap-theme.css" />
-<link rel ="stylesheet" href ="css/bootstrap-theme.min.css" />
-<link rel ="stylesheet" href ="css/bootstrap.css" />
-<link rel ="stylesheet" href ="css/bootstrap.min.css" />
-<script src="js/bootstrap.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
--->
-    <link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
-    <link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
+<link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
 
-    <script type='text/javascript' src='jquery/min.js'></script>
-    <script type='text/javascript' src='jquery/ui-min.js'></script>
-    <script type="text/javascript" src='common/mukoni.jquery.js'></script> 
-    <script type="text/javascript" src='common/mukoni.jquery-ui.js'></script> 
-    <?php
-      if ($_GET['c'] != 'logout') { 
-        require_once('session.php');
-        global $session;
-        if ($session->user != null) { ?>
-      <script>
-/*        $(function(){
-          var timer = null;
-          var time=1000*60*60
-          var checker = function(){
-              if(timer){clearTimeout(timer);} // cancels the countdown.
-              timer=setTimeout(function() {
-                  window.location.href="logout.html?&event=timeout";
-              },time); // reinitiates the countdown.
-          };
-          checker(); // initiates the countdown.
-          // bind the checker function to user events.
-      $(document).bind("mousemove keypress click", checker); 
-      }); */
-    </script> 
-   <?php }
-   }  ?>
- </head>
+<script type='text/javascript' src='jquery/min.js'></script>
+<script type='text/javascript' src='jquery/ui-min.js'></script>
+<script type="text/javascript" src='common/mukoni.jquery.js'></script> 
+<script type='text/javascript' src="common/page.js"></script>
+<!--<script type="text/javascript" src='common/mukoni.jquery-ui.js'></script>-->
+
+<?php
+  require_once ('log.php');
+  require_once ('session.php');
+  $page = $_GET['page'];
+
+  function pre_load_custom($page)
+  {
+    log::debug("loading page $page");
+    if (file_exists("pre-$page.php")) {
+      require_once "pre-$page.php";
+    } 
+    if (file_exists("$page.css")) { 
+      echo "<link type='text/css' rel='stylesheet' href='$page.css'></link>";
+    } 
+    if (file_exists("$page.js")) { 
+      echo "<script type='text/javascript' src='$page.js'></script>";
+    }
+  }
+
+  global $session;
+  log::init('index', log::DEBUG);
+  log::debug(json_encode($session));
+  log::debug("content is '".$_SESSION['content']."'");
+  $public_pages = array('login_page','home','map','logout');
+  if (!$session && !in_array($_GET['content'],$public_pages) && !in_array($page, $public_pages) ) {
+    $page = 'landing';
+  }
+  else if (isset($_GET['content'])) {
+    $content = $_SESSION['content'] = $_GET['content'];
+  }
   
-  <body>
-    <div id='frame'>
-      <div id='header'>
-        <?php load_div('banner'); ?>
-        <?php load_div('menu'); ?>
-      </div>
-      <div id='body'>
-        <?php load_div('left-nav'); ?>
-        <?php load_div('content'); ?>
-        <?php load_div('right-nav'); ?>
-      </div>
-      <?php load_div('footer'); ?>
-   </div>
-  </body>
-</html>
+  else if (isset($_SESSION['content'])) {
+    $content = $_SESSION['content'];
+  }
+  else 
+    $page = 'landing';
+  
+  pre_load_custom($page);
+  if (isset($content)) 
+    pre_load_custom($content);
+  
+  $params = array_merge($_GET, array('page'=>$page));
+?>
+<script>
+$(function() {
+  $("body").page({data: <?=json_encode($params);?>});
+});
+</script>
