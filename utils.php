81af7f8111255ec879cb52ff3af1df91a98a7124
fhulu <fhulu>
2014-09-23 03:30:56 +0000

    - addes set_valid()

--- utils.php
+++ utils.php
@@ -9,8 +9,10 @@ function GET($item) { return at($_GET, $item); }
 function POST($item) { return at($_POST, $item); }
 function REQUEST($item) { return at($_REQUEST, $item); }
 function SESSION($item) { return at($_SESSION, $item); }
-
-function last($array)
+function last($array) { return at($array, sizeof($array)-1); }
+function null_at($array, $index) { return is_null(at($array,$index)); }
+function set_valid(&$dest, $source, $index) 
 {
-  return at($array, sizeof($array)-1);
+  $val = $source[$index];
+  if (!is_null($val)) $dest[$index] = $val;
 }
