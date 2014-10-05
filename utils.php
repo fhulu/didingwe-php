8be00e2e2b9baab88b2cee88718b9a251652c900
fhulu <fhulu>
2014-10-05 20:46:16 +0000

    - added error handler

--- utils.php
+++ utils.php
@@ -81,3 +81,11 @@ function compress_array($array)
   return $compressed;
 }
   
+function errHandle($errNo, $errStr, $errFile, $errLine) {
+  $msg = "$errStr in $errFile on line $errLine";
+  if ($errNo == E_NOTICE || $errNo == E_WARNING) 
+    throw new ErrorException($msg, $errNo);
+  echo $msg;
+}
+
+set_error_handler('errHandle');
