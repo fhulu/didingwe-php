0c4546fbc099c6add38543f8191847219a94a959
fhulu <fhulu>
2014-10-07 15:39:15 +0000

    removed unsed css and allow breach to be accessible to public

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-  require_once('../common/utils.php');
-  require_once('../common/session.php');
+  require_once('utils.php');
+  require_once('session.php');
   if (isset($_GET['a'])) {
-    require_once('../common/action.php');
+    require_once('action.php');
     return;
   }
 ?>
@@ -11,9 +11,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link href="default.style.css" media="screen" rel="stylesheet" type="text/css" />	
 <link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
-<link href="jquery/jquery-ui.min.css" media="screen" rel="stylesheet" type="text/css" />	
 <link href="jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
-<link href="jquery/jquery-ui.theme.min.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="common/jquery.datetimepicker.css" media="screen" rel="stylesheet" type="text/css" />
 
 <script type='text/javascript' src='jquery/min.js'></script>
@@ -27,13 +25,12 @@
   var request_method = '<?=config::$request_method;?>';
 </script>
 <?php
-  require_once ('../common/log.php');
+  require_once ('log.php');
   
   $page = GET('page');
 
   function pre_load_custom($page)
   {
-    log::debug("loading page $page");
     if (file_exists("$page.php")) {
       require_once "$page.php";
     } 
@@ -47,12 +44,8 @@
 
   global $session;
   log::init('index', log::DEBUG);
-  log::debug(json_encode($session));
+  $public_pages = array('login','map','register','error_page','forgot_password', 'otp_page','otp_done','breach');
   $content = GET('content');
-  if (!is_null($content) && !in_array($content, array('landing','login')))
-    $_SESSION['content'] = $content;
-
-  $public_pages = array('login','map','register','error_page');
   if (!$session && !in_array($content,$public_pages) && !in_array($page, $public_pages) ) {
     $page = 'landing';
   }
