b9aa19bef8004b813ad8c5fea83336f557b9a4ec
fhulu <fhulu>
2014-10-25 17:44:21 +0000

    - fixed restore previous page after login

--- user.php
+++ user.php
@@ -1095,9 +1095,9 @@ class user
       page::error("email", "Invalid username/password for '$email'");
     else {
       $page = SESSION('content');
-      if (is_null($page)) $page = '/home';
+      if (is_null($page)) $page = 'home';
       page::close_dialog();
-      page::redirect("$page");
+      page::redirect("/$page");
     }
   }
  
