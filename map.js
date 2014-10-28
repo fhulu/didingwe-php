1b5444fbdaa8ca65994d76fa269f19bf859fd97d
lux <lux>
2014-10-28 08:22:05 +0000

    - listens for customValue event to set coordinates

--- map.js
+++ map.js
@@ -1,38 +1,39 @@
-(function( $ ) {
-  $.widget( "ui.mapper", {
-  options: {
-     latitude: -33.884322,
-     longitude: 18.632458,
-     zoom: 8,
-     pin: null,
-     value: null,
-     marker: null,
-   },
-     
-    _create: function() 
-    {
-      this.location(this.options.latitude, this.options.longitude);
+(function($) {
+  $.widget("ui.mapper", {
+    options: {
+      latitude: -29.801842,
+      longitude: 30.592950,
+      zoom: 8,
+      pin: null,
+      value: null,
+      marker: null,
     },
-    
+    _create: function()
+    {
+      var self = this;
+      this.element.on('customValue', function(event, value) {
+        self.val(value);
+      })
+      this.show();
+    },
     show: function()
     {
       this.options.zoom = parseInt(this.options.zoom);
       var props = {
-          center: this.position,
-          zoom: this.options.zoom,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+        center: this.position,
+        zoom: this.options.zoom,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
       this.map = new google.maps.Map(document.getElementById(this.element.attr('id')), props);
       this.map.setZoom(this.options.zoom);
       this.marker(google.maps.Animation.DROP);
     },
-    
     location: function(latitude, longitude, show)
     {
       this.position = new google.maps.LatLng(latitude, longitude);
-      if (show === undefined || show === true) this.show();
+      if (show === undefined || show === true)
+        this.show();
     },
-    
     val: function(value)
     {
       if (value === undefined) {
@@ -40,16 +41,17 @@
       }
       var position = value.split(',');
       this.location(position[0], position[1]);
+      return this;
     },
-    
     marker: function(value)
     {
-      if (value === undefined) return this.marker;
-      this.options.marker = new google.maps.Marker({
-          map: this.map,
-          position: this.position,
-          animation: value
-        });
-    }
+      if (value === undefined)
+        return this.marker;
+        this.options.marker = new google.maps.Marker({
+        map: this.map,
+        position: this.position,
+        animation: value
+      });
+    },
   })
 })(jQuery);
