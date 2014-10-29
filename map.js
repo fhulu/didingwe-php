85609fb7187687ca2d3912f573f03bb82a6f69a2
lux <lux>
2014-10-29 09:06:38 +0000

    - added multiple values on maps

--- map.js
+++ map.js
@@ -7,15 +7,54 @@
       pin: null,
       value: null,
       marker: null,
+      hint: null
     },
     _create: function()
     {
       var self = this;
       this.element.on('customValue', function(event, value) {
         self.val(value);
-      })
+      });
+      if (this.options.center)
+        this.location(this.options.center[0], this.options.center[1]);
+      
+      if (this.options.load) {
+        var data = { action: 'data', path: this.options.path + '/load' };
+        $.json('/', {data: data}, function(data) {
+          self.addPoints(data);
+        });
+      }
       this.show();
     },
+    
+    addPoints: function(data)
+    {
+      var colors = this.options.colors;
+      console.log(colors)
+      var hint = this.hint;
+      var self = this;
+      var map = this.map;
+      var icon_path = this.options.icon_path;
+      data.forEach(function(value) {
+        var position = new google.maps.LatLng(parseFloat(value[0]), parseFloat(value[1]));
+        var color = value[2];
+        var icon = icon_path+colors[color]+'-dot.png';
+        var marker = new google.maps.Marker({
+          position: position,
+          map: self.map,
+          icon: icon
+        });
+        google.maps.event.addDomListener(marker, 'mouseover', function() {
+          hint = new google.maps.InfoWindow({content: value[3]});
+          hint.open(self.map, marker);
+        });
+        google.maps.event.addDomListener(marker, 'mouseout', function() {
+          if (hint !== null)
+            hint.close();
+        });
+      });
+    },
+    
     show: function()
     {
       this.options.zoom = parseInt(this.options.zoom);
