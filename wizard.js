0a8be822ed87ab5edd281e7eb56b2af42d670eea
fhulu <fhulu>
2015-04-13 23:48:23 +0000

    - dynamic pages

--- wizard.js
+++ wizard.js
@@ -2,128 +2,121 @@
   $.widget( "ui.wizard", {
     _create: function() {
       this.stack = new Array();   
-      this.dialogs = new Array();
-      this.last_url = undefined;
       var self = this;
-      $.each(this.element.children(), function(i) {
-        var dialog = $(this);
-        var div = dialog.dialog({
-          modal: true,
-          autoOpen: false,
-          width: parseInt($(this).css('width')),
-          //note: show commented out, using show causes an exception if div contains input[type=file]
-//          show: 'blind', 
-          hide: 'explode'
-        });
-        self.dialogs.push(div);
-        
-        $(this).find("[wizard='next']").click(function() {
-          var steps = $(this).attr('steps');
-          self.goNext(steps);
-        });
-
-        $(this).find("[wizard='back']").click(function() {
-          self.goBack();
-        });
-        
-        $(this).find("[wizard='close']").click(function() {
-          if (self.last_url != undefined)
-            window.location.href = self.last_url;
-          dialog.dialog('close');
-        });
-        
-      });
-
-    },
-    
-    onNext: function(index, callback)
-    {
-      var dialog = this.element.children().eq(index);
-      dialog.find("[wizard='next']").click(callback);
-    },
-    
-    dialog: function(selector) { 
-      return $(this).find(selector); 
-    },
-    
-    start: function(index) {
-      if (index == undefined) index = 0;
+      this.width =  this.options.width;
+      var num_steps = this.options.steps.length;
+      this.page_width = this.options.width - (num_steps*10);
+      this._createPages();
+      this._bindActions();
       this.stack = new Array();
-      for (var i=0; i < index; i++)
-        this.stack.push(1);
-      this.dialogs[index].dialog('open');
-      return false;
+      this._showPage(0);
     },
     
-    goNext: function(steps) {
-      steps = steps==undefined? 1: parseInt(steps);
-      var cur_idx = 0;
-      var self = this;
-      $.each(self.stack, function(i, v) {
-        cur_idx += parseInt(v);
-      });
-  
-      $.each(self.dialogs, function(i) {
-        var dialog = this.dialog().data('dialog');
-        if (i == cur_idx) {
-          if (dialog.isOpen()) { 
-            self.auto_closed = true;
-            dialog.close();
-          }
-          else {
-            self.auto_closed = false;
-            dialog.open();
-            return false;
-          }   
-          // make sure we remember to go back the same number of times we went forward
-          cur_idx += steps;
-          self.stack.push(steps);
-        }
-      });
-    },
-    
-    goBack: function() {
-      var cur_idx = 0;
-      $.each(this.stack, function(i, v) {
-        cur_idx += v;
-      });
-         
-      $.each(this.dialogs, function(i) {
-        if (i == cur_idx) {
-            self.auto_closed = true;
-          this.dialog('close');
-          return false;
-        }
-      });
-   
-      cur_idx -= this.stack.pop();
-      $.each(this.dialogs, function(i) {
-        if (i == cur_idx) {
-            self.auto_closed = false;
-          this.dialog('open');
-          return false;
-        }
-      });
-    },
-
-    close: function() 
+    _createPages: function()
     {
-      var cur_idx = 0;
       var self = this;
-      $.each(self.stack, function(i, v) {
-        cur_idx += parseInt(v);
-      });
-  
-      this.dialogs[cur_idx].dialog('close');
+      $.each(this.options.steps, function(i, info) {
+        var name = info;
+        var props = {};
+        if ($.isPlainObject(name)) {
+          for (var k in info) {
+            name = k;
+            props = info[k];
+            break;
+          }
+        }
+        props.name = name;
+        self.options.steps[i] = props;
+        self._createPage(i);
+      })
+    },
+    
+    _createPage: function(index) 
+    {
+      var props = this.options.steps[index];
+      var page = $('<div class=wizard-page>').attr('name',props.name).appendTo(this.element);
+      var heading = $('<div class=wizard-heading>').appendTo(page);
+      $('<div class=wizard-number>').text(index).appendTo(heading);
+      $('<span class=wizard-title>').appendTo(heading);
+    },
+    
+    _showPage: function(index)
+    { 
+      if (this.stack.length) {
+        var prev_page;
+        var prev_index = this.stack[this.stack.length-1];
+        if (prev_index < index) {
+           var prev_page = this.element.find('.wizard-page').eq(prev_index);
+           prev_page.removeClass('wizard-current');
+           prev_page.addClass('wizard-done');
+        }
+        else do { 
+          prev_page = this.element.find('.wizard-page').eq(prev_index);
+          prev_page.removeClass('wizard-done');
+          prev_index = this.stack.pop();
+        } while (prev_index > index);
+        prev_page.find('.wizard-content,.wizard-nav').hide();
+        
+      }
+
+      var page = this.element.find('.wizard-page').eq(index);
+      page.addClass('wizard-current');
+      var content = page.find('.wizard-content');
+      if (!content.exists()) 
+        this._loadPage(page, index);
+      else content.show();
+      this.stack.push(index);
+      page.find('.wizard-nav').show();
     },
         
-    bind: function(event, callback) 
+    _loadPage: function(page, index)
     {
-      $.each(this.dialogs, function() {
-        $(this).bind( event, function(evt, ui) {
-          callback(evt, ui);
-        });
+      page.addClass('wizard-loading');
+      var props = this.options.steps[index];
+      var path = this.options.path;
+      path = path.substr(0, path.lastIndexOf('/')+1) + props.name;
+      var tmp = $('<div></div>');
+      tmp.page({path: path, key: this.options.key});
+      var self = this;
+      tmp.on('read_'+path.replace(/\//, '_'), function(event, object) {
+        object.css('height','100%');
+        object.addClass('wizard-content').appendTo(page);
+        self._createNavigation(page, props, index);
+        page.addClass('wizard-loaded').removeClass('wizard-loading');
       });
-    }
+    },
+    
+    _createNavigation: function(parent, props, index)
+    {
+      var num_steps = this.options.steps.length;
+      if (props.prev === undefined) props.prev = index > 0 && num_steps > 1;
+      if (props.next === undefined) props.next = index >= 0 && index < num_steps-1;
+      var nav = $('<div class=wizard-nav>').appendTo(parent);
+      if (!props.prev && !props.next) return;
+      if (props.prev) 
+        $('<button class="wizard-prev action">').text(this.options.prev_name).appendTo(nav);
+      if (props.next) 
+        $('<button class="wizard-next action">').text(this.options.next_name).appendTo(nav);
+      var self = this;
+      nav.find('.wizard-prev').click(function() {
+        nav.trigger('wizard-jump', [self.stack[self.stack.length-2]]);
+      })
+      nav.find('.wizard-next').click(function() {
+        if (typeof props.next === 'string') {
+          var page = self.element.find('.wizard-page[name="'+props.next+'"]');
+          index = self.element.find('.wizard-page').index(page);
+        }
+        else index += 1;
+        nav.trigger('wizard-jump', [index]);
+      })
+    },
+    
+    _bindActions: function()
+    {
+      var self = this;
+      this.element.on('wizard-jump', function(event, index) {
+        self._showPage(index);
+      })
+    }    
   })
 }) (jQuery);
