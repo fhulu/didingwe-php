2b9bfd7fbf87e6783e63b476b3c1e8d14f606fd3
Fhulu Lidzhade <fhulu@mukoni.co.za>
2015-07-31 10:36:34 +0200

    pre-include input_page.css to prevent page flicker

--- index.php
+++ index.php
@@ -19,28 +19,29 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<link href="/jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />	
-<link href="/default.style.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />	
+<link href="/jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="/common/input_page.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
+<link href="/default.style.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
 
 <script type='text/javascript' src='/jquery/min.js'></script>
 <script type='text/javascript' src='/jquery/ui-min.js'></script>
-<script type="text/javascript" src='/common/mukoni.jquery.js?<?=$tag?>'></script> 
+<script type="text/javascript" src='/common/mukoni.jquery.js?<?=$tag?>'></script>
 <script type='text/javascript' src="/common/page.js?<?=$tag?>"></script>
 <script>
   var request_method = '<?=config::$request_method;?>';
 </script>
 <?php
   require_once ('../common/log.php');
-  
+
   function pre_load_custom($page)
   {
     log::debug("loading page pre_$page");
     $file = "pre_$page.php";
     $common_file = "../common/$file";
-    if (file_exists($file)) 
+    if (file_exists($file))
       require_once $file;
     else if (file_exists($common_file))
-      require_once $common_file;    
+      require_once $common_file;
   }
 
   global $session;
@@ -53,7 +54,7 @@
   if ($content == '') $content = 'home';
   if ($page == '') $page = 'index';
   if (!is_null($page))  pre_load_custom($page);
-  if ($content != $page && !is_null($content)) 
+  if ($content != $page && !is_null($content))
     pre_load_custom($content);
   $request = array_merge($_REQUEST, array('content'=>$content));
   $options = array("path"=>$page, 'request'=>$request)
