7de3bb5c9f62cc990d06fe34bef68ccbd6531f7c
Fhulu Lidzhade <fhulu@mukoni.co.za>
2015-10-26 10:55:05 +0200

    simplified db configuration

--- db.php
+++ db.php
@@ -68,8 +68,6 @@ class db
   function exec($q, $max_rows=0, $start=0)
   {
     global $config;
-    $q = str_replace('$pid', $config['program_id'], $q);
-    $q = str_replace('$audit_db', $config['audit_db'], $q);
     if ($start=='') $start = 0;
     if ($max_rows > 0) $q .= " limit $start, $max_rows";
     log::debug("SQL: $q");
@@ -137,9 +135,8 @@ class db
   static function init_default()
   {
     global $db,$config;
-    $default_db = isset($config['default_db'])?$config['default_db']:$config['audit_db'];
     if ($db == null) {
-      $db = new db($default_db, $config['audit_user'], $config['audit_passwd']);
+      $db = new db($config['default_db'], $config['db_user'], $config['db_passwd']);
     }
   }
 
