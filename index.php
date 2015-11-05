75b443262cb7da38280a6fbb947baec6fe1bd379
Tshepo Morailane <tshepo.morailane@mukoni.net>
2015-11-05 13:18:11 +0200

    Added Bootstrap 3.3.5 Distro

--- index.php
+++ index.php
@@ -19,7 +19,21 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link href="/jquery/smoothness/ui.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="/common/jquery.datetimepicker.css" media="screen" rel="stylesheet" type="text/css" />
+
+<!-- Bootstrap -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
 <link href="/common/input_page.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
+
 <link href="/default.style.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
 <script type='text/javascript' src='/jquery/min.js'></script>
 <script type='text/javascript' src='/jquery/ui-min.js'></script>
