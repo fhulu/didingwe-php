0e9dddd144fcacf2e79e4f66d9244626e88b8699
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-04-24 01:21:05 +0200

    revert link and css caching

--- mkn.render.js
+++ mkn.render.js
@@ -755,15 +755,6 @@ mkn.render = function(options)
 
   var loadLink = function(link,type, callback)
   {
-    var cache = mkn.links[link];
-    if (cache !== undefined) {
-      if (cache.loaded)
-        callback();
-      else
-        cache.callbacks.push(callback);
-      return;
-    }
-    mkn.links[link] = {loaded: false, callbacks: [callback]};
     var element;
     if (type == 'css') {
       element = document.createElement('link');
@@ -777,13 +768,10 @@ mkn.render = function(options)
       element.src =  link;
       element.type = 'text/javascript';
     }
-    element.onreadystatechange = element.onload = function() {
-      cache = mkn.links[link];
-      cache.loaded = true;
-      while (cache.callbacks.length) {
-        var func = cache.callbacks.shift();
-        func();
-      }
+    var loaded = false;
+    if (callback !== undefined) element.onreadystatechange = element.onload = function() {
+      if (!loaded) callback();
+      loaded = true;
     }
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(element);
