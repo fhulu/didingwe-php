68fa0431fcb08fa3cf5235794ebdf8bccdb346c8
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-06-21 00:41:15 +0200

    use w3.css and google material icons on index

--- index.php
+++ index.php
@@ -17,17 +17,9 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<link href="/jquery/ui.min.css" media="screen" rel="stylesheet" type="text/css" />
-<link href="/common/jquery.datetimepicker.css" media="screen" rel="stylesheet" type="text/css" />
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-
+<link href="/common/google-material-icons.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
+<link href="/common/w3.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
 <link href="/common/input_page.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
 <link href="/input_page.css?<?=$tag?>" media="screen" rel="stylesheet" type="text/css" />
 
