2f75816d7bd2c6c06dffb76f9f9ae6c79a532fc4
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-07-24 21:24:13 +0200

    started working on google custome search engine integration

---
+++ gcse.js
@@ -0,0 +1,10 @@
+$(function() {
+  var cx = '006040751555522576989:fc20hvgikg';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+});
