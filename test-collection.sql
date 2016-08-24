d3f72d93cf0f83bb03a15af9016bf9c14545e2d8
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-08-24 19:37:57 +0200

    added collection test sql

---
+++ test-collection.sql
@@ -0,0 +1,19 @@
+select u.identifier, c.identifier, c.value
+from collection u join collection c on u.collection='user' and c.collection = 'country'
+and u.attribute = 'country' and u.value = c.identifier
+
+select u.identifier,  ufn.value first_name, uln.value last_name, ul.value language_code, l.value language, u.create_time
+from collection u
+left join collection ufn on ufn.collection = 'user' and ufn.version = u.version and ufn.identifier = u.identifier and ufn.attribute = 'first_name'
+left join collection uln on uln.collection = 'user' and uln.version = u.version and uln.identifier = u.identifier and uln.attribute = 'last_name'
+left join collection ul on ul.collection = 'user' and ul.version = u.version  and ul.identifier = u.identifier and ul.attribute = 'language'
+left join collection l on l.collection = 'language' and l.version = u.version and ul.value = l.identifier and l.attribute = 'name'
+where u.collection = 'user' and u.attribute = 'password' and u.version = 0
+
+select identifier,
+(select value from collection where collection = m.collection and version <= m.version and identifier=m.identifier and attribute = 'first_name' order by version desc limit 1) first_name,
+(select value from collection where collection = m.collection and version <= m.version and identifier=m.identifier and attribute = 'last_name' order by version desc limit 1) last_name,
+(select value from collection where collection = m.collection and version <= m.version and identifier=m.identifier and attribute = 'language' order by version desc limit 1) language_code,
+create_time
+from collection m
+where collection = 'user' and attribute = 'password' and version = 0
