67bdf0cf9a4a9aa2be1460918a31b034f9dc59f4
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-10-04 05:08:16 +0200

    derive from parent before merging

--- mkn.render.js
+++ mkn.render.js
@@ -200,7 +200,7 @@ mkn.render = function(options)
       var item = items[i];
       var id;
       var array;
-      var template; 
+      var template;
       removed.push(i);
       if ($.isPlainObject(item)) {
         if (setDefaults(defaults, item, parent_field)) continue;
@@ -380,7 +380,6 @@ mkn.render = function(options)
       field = mkn.merge(field.template.subject, field);
       if (!field.template.tag) delete field.template;
     }
-    field = this.mergeType(field);
     deriveParent(parent, field);
     field = this.parentSow(parent, field);
     field = this.mergeType(field);
