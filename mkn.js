9439e2d0cd59b56acdd3efb93d53e8c78c638e6b
Fhulu Lidzhade <fhulu@mukoni.co.za>
2016-12-20 07:33:10 +0200

    added escapeHtml() js from stackoverflow

--- mkn.js
+++ mkn.js
@@ -378,6 +378,19 @@ var mkn = new function() {
       document.head.appendChild(element);
     }).promise();
   }
+
+  // from stackoverflow http://stackoverflow.com/questions/1787322/htmlspecialchars-equivalent-in-javascript/4835406#4835406
+  this.escapeHtml = function(text) {
+    var map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+  }
 }
 
 if (!String.prototype.trim) {
