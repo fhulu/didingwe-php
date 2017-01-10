e2e9b8396fda0618130e42aabd7d5f854b18292b
Fhulu Lidzhade <fhulu@mukoni.co.za>
2017-01-10 20:28:34 +0200

    allow datatable row actions to be customized at control instance level

--- datatable.js
+++ datatable.js
@@ -431,7 +431,9 @@
         row_actions.push('slideoff');
 
       td.addClass('actions');
-      var all_actions = this.options.row_actions;
+
+      var opts = this.options;
+      var all_actions = opts.row_actions;
       var key = tr.attr('key');
       var normal_actions = [];
       var slide_actions = [];
@@ -441,12 +443,13 @@
         var id = action.id;
         if (row_actions.indexOf(id) < 0) continue;
         action.key = key;
+        action = mkn.merge(action, opts[id]);
         actions.push(action);
         if (id == 'slide')
           actions = slide_actions;
       }
       if (normal_actions.length)
-        this.options.render.createItems(td, {}, undefined, normal_actions);
+        opts.render.createItems(td, {}, undefined, normal_actions);
       if (!slide_actions.length) return;
       var slider = $('<div class="slide">').toggle(false).appendTo(td);
       slider.data('actions', slide_actions);
