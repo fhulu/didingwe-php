2971ed3bc3fecccbeed8c8af20442943d35c19ce
Fhulu Lidzhade <fhulu@mukoni.co.za>
2017-01-10 22:48:39 +0200

    reset isearch value on search input change

--- isearch.js
+++ isearch.js
@@ -14,7 +14,7 @@ $.widget( "custom.isearch", {
     var opts = me.options;
     me.params = { action: 'data', path: opts.path, key: opts.key, offset: 0, size: opts.drop.autoload  };
     me.searcher = el.find('.isearch-searcher').on('keyup input cut paste', function() {
-      if (me.params.term == $(this).val()) return;
+      el.val("")
       me.params.offset = 0;
       me.drop.show();
       me._load();
