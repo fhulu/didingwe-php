04a0c8df39a640c2bde87d0b6a4f5c56f6dbf24c
Fhuu Lidzhade <fhulu@mukoni.co.za>
2017-01-11 14:05:39 +0200

    allow of send sms with cellphone prefixed with +

--- sms.php
+++ sms.php
@@ -6,10 +6,13 @@ class sms {
     $this->page = $page;
   }
   function send($options) {
+
     $this->page->merge_fields($options);
     $this->page->replace_fields( $options['message']);
     $options['message'] = urlencode($options['message']);
     $this->page->merge_context('sms.send', $options);
+    replace_fields($options, $options, true);
+    $options['msisdn'] = urlencode($options['cellphone']);
     replace_fields($options, $options, true);
     $url = $options['url'];
     if (!isset($url)) {
