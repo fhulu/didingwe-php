519f81a9839b82f210d26a4a4b88cd8e148ab480
Fhulu Lidzhade <fhulu@mukoni.co.za>
2017-02-13 15:13:54 +0200

    limit multiple merge fields and allow key variable substitution

--- page.php
+++ page.php
@@ -355,6 +355,7 @@ class page
       if (is_assoc($field)) {
         $new_parent = $field;
         $field = $field[$branch];
+        // $this->merge_fields($field, $parent);
         if ($parent)
           $this->derive_parent($parent, $field);
         $parent = $new_parent;
@@ -536,8 +537,11 @@ class page
   function merge_fields(&$fields, $parent=[], $merged=[])
   {
     if (is_assoc($fields)) {
-      if (isset($fields['type']))
+      $type = $fields['type'];
+      if (isset($type) && !in_array($type, $merged, true)) {
+        $merged[] = $type;
         $this->merge_type($fields);
+      }
       $this->derive_parent($parent, $field);
       foreach($fields as $key=>&$value) {
         if (!is_array($value) || page::not_mergeable($key)) continue;
@@ -573,6 +577,7 @@ class page
       }
       if (!is_null($default))
         $field = merge_options($default, $field);
+      unset($field['type']);
       if (is_array($field))
         $value = array($key=>$field);
     }
@@ -852,7 +857,7 @@ class page
     $invoker = $this->context;
     if (!isset($this->context['id'])) $this->context['id'] = last($this->path);
     if (!isset($this->context['name'])) $this->context['name'] = $this->name($this->context);
-    $this->merge_fields($this->fields);
+    // $this->merge_fields($this->fields);
     $validate = at($invoker, 'validate');
     if ($validate != 'none' && !$this->validate($this->fields, $validate))
       return null;
@@ -862,7 +867,7 @@ class page
     $result = $this->reply($invoker);
     if (!$audit_first && !page::has_errors() && array_key_exists('audit', $invoker))
       $this->audit($invoker);
-    return $this->answer;
+    return $result;
   }
 
   function replace_sid(&$str)
@@ -1094,7 +1099,6 @@ class page
     if (isset($post)) $actions = $post;
     if (is_null($actions)) return null;
     if (is_assoc($actions))  $actions = array($actions);
-    $this->merge_fields($actions, $this->fields);
 
     log::debug_json("REPLY ACTIONS", $actions);
 
@@ -1293,10 +1297,12 @@ class page
     $this->parse_delta($vars);
     $session = &$_SESSION['variables'];
     foreach($vars as $var) {
+      $this->replace_fields($var);
       if ($var == 'request' && !isset($this->request['request']))
         call_user_func_array (array($this, 'write_session'), array_keys($this->request));
       else if (is_array($var)) {
         list($var,$value) = assoc_element($var);
+        $this->replace_fields($var);
         $session[$var] = $value;
       }
       else if (isset($this->answer[$var]))
@@ -1306,12 +1312,13 @@ class page
     }
   }
 
-  static function read_settings($settings,$args)
+  function read_settings($settings,$args)
   {
     $vars = page::parse_args($args);
     $values = array();
     foreach($vars as $var) {
       $alias = $var;
+      $this->replace_fields($var);
       if (is_array($var))
         list($alias,$var) = assoc_element($var);
       if (isset($settings[$var]))
@@ -1325,7 +1332,7 @@ class page
     $args = func_get_args();
     $session = &$_SESSION['variables'];
     if (sizeof($args) == 0) return $session;
-    return page::read_settings($session, $args);
+    return $this->read_settings($session, $args);
   }
 
   function read_session_list()
@@ -1337,9 +1344,7 @@ class page
   function read_config()
   {
     global $config;
-    $result = page::read_settings($config, func_get_args());
-    log::debug_json("result", $result);
-    return $result;
+    return $this->read_settings($config, func_get_args());
   }
 
   function read_values($values)
