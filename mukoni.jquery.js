6d1a7024e7b48ba36bf85c85707a5d33c3638917
Fhulu Lidzhade <fhulu@mukoni.co.za>
2017-05-24 11:58:43 +0200

    don't escape html for elements with parseHtml set to 1

--- mukoni.jquery.js
+++ mukoni.jquery.js
@@ -49,7 +49,7 @@ $.fn.setValue = function(val)
     return this;
   }
 
-  if (typeof val == 'string')  val = mkn.escapeHtml(val);
+  if (typeof val == 'string' && !this.attr('parseHtml'))  val = mkn.escapeHtml(val);
   if (this.hasAttr('value'))
     return this.val(val);
   return this.html(val);
