1f47cdcd9b5df72a5451b491f28572c0f172a78b
Fhulu Lidzhade <fhulu@mukoni.co.za>
2017-06-10 01:39:56 +0200

    added collection sql query scenarios

---
+++ collection.sql
@@ -0,0 +1,93 @@
+-- select time_added, cellphone, contact_info, access_level from contact
+select identifier, attribute,value from contact
+  where collection = 'contact' and attribute in ('time_added','cellphone','contact_info', 'access_level')
+  order by identifier
+--0.0005 seconds
+
+-- select time_added, cellphone, contact_info, access_level from contact order by cellphone
+select contact.identifier, contact.attribute, contact.value from contact `contact`
+  left join contact sorter
+      on `contact`.collection = sorter.collection and `contact`.identifier = sorter.identifier and sorter.attribute = 'cellphone'
+  where contact.collection = 'contact' and contact.attribute in ('time_added','cellphone','contact_info', 'access_level')
+  order by sorter.value, contact.identifier
+   --0.0470 seconds
+
+ select contact.identifier, contact.attribute, contact.value from contact `contact`
+   left join (select value, identifier from contact where collection = `contact`.collection and attribute = 'cellphone'
+        ) sorter
+       on `contact`.identifier = sorter.identifier
+   where contact.collection = 'contact' and contact.attribute in ('time_added','cellphone','contact_info', 'access_level')
+   order by sorter.value, contact.identifier
+      --0.0470 seconds
+
+-- select time_added, cellphone, contact_info, access_level from contact where owner = 527
+select `contact`.identifier, `contact`.attribute, `contact`.value
+  from contact `contact`
+    join contact `owner`
+      on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+      and `owner`.attribute = 'owner' and `owner`.value = 527
+    where `contact`.collection = 'contact' and `contact`.attribute in ('time_added','cellphone','contact_info','access_level')
+  order by 1
+
+-- select time_added, cellphone, contact_info, access_level from contact where owner = 527 order by cellphone
+select `contact`.identifier, `contact`.attribute, `contact`.value
+  from contact `contact`
+    join contact `owner`
+      on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+      and `owner`.attribute = 'owner' and `owner`.value = 527
+      left join contact sorter
+          on `contact`.collection = sorter.collection and `contact`.identifier = sorter.identifier and sorter.attribute = 'cellphone'
+    where `contact`.collection = 'contact' and `contact`.attribute in ('time_added','cellphone','contact_info','access_level')
+  order by sorter.value, contact.identifier
+  --0.0148 seconds
+
+    --0.0407 seconds
+
+-- select time_added, cellphone, contact_info, access_level,  if(blacklisted,'whitelist','blacklist') actions, from contact where owner = 527
+select `contact`.identifier, `contact`.attribute, `contact`.value
+  from contact `contact`
+   join contact `owner`
+      on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+      and `owner`.attribute = 'owner' and `owner`.value = 527
+    where `contact`.collection = 'contact' and `contact`.attribute in ('time_added','cellphone','contact_info','access_level')
+  union
+  select `contact`.identifier, 'actions' attribute, if(`contact`.value,'whitelist','blacklist')
+    from contact `contact`
+     join contact `owner`
+        on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+        and `owner`.attribute = 'owner' and `owner`.value = 527
+      where `contact`.collection = 'contact' and `contact`.attribute in ('blacklisted')
+    order by 1
+  --0.0324 seconds
+
+  select `contact`.identifier,
+     if(`contact`.attribute='blacklisted','actions', `contact`.attribute) `attribute`, if(`contact`.attribute='blacklisted',
+        if(`contact`.value,'whitelist','blacklist'),contact.value) `value`
+    from contact `contact`
+      join contact `owner`
+        on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+        and `owner`.attribute = 'owner' and `owner`.value = 527
+    where `contact`.collection = 'contact' and `contact`.attribute in ('time_added','cellphone','contact_info','access_level', 'blacklisted')
+    order by 1
+    --0.0187
+
+  -- select time_added, cellphone, contact_info, access_level,  if(blacklisted,'whitelist','blacklist') actions, from contact where owner = 527
+  select `contact`.identifier,
+     if(`contact`.attribute='blacklisted','actions', `contact`.attribute) `attribute`, if(`contact`.attribute='blacklisted',
+        if(`contact`.value,'whitelist','blacklist'),contact.value) `value`
+    from contact `contact`
+      join contact `owner`
+        on  `owner`.collection = `contact`.collection and `contact`.identifier = `owner`.identifier
+        and `owner`.attribute = 'owner' and `owner`.value = 527
+    left join contact sorter
+        on `contact`.collection = sorter.collection and `contact`.identifier = sorter.identifier and sorter.attribute = 'cellphone'
+    where `contact`.collection = 'contact' and `contact`.attribute in ('time_added','cellphone','contact_info','access_level', 'blacklisted')
+    order by sorter.value, contact.identifier
+    --0.0500 seconds
+
+-- select time, action, detail from audit where action like 'log%'
+select value from session
+  where collection = 'audit' and attribute in ('time','action','detail')
+  and (attribute != 'action' or attribute = 'action' and value like '%log%')
+  order by identifier
+-- 0.0008 seconds
