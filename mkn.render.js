21816df728c887bcb84b7159fec7d01a5319efcd
Fhulu Lidzhade <fhulu@mukoni.co.za>
2019-07-24 08:40:45 +0200

    fixed faulty merge

--- mkn.render.js
+++ mkn.render.js
@@ -823,10 +823,15 @@ mkn.render = function(options)
 
   var initClassToggle = function(obj, field) {
     var args = getArray(field.toggles);
+    var class1, class2, event = 'click';
     switch(args.length) {
       case 0: return;
+      case 1: class1 = args[0]; break;
+      case 2: class1 = args[0]; class2 = args[1]; break;
+      default: class1 = args[0]; class2 = args[1]; event = args[3];
     }
     obj.on(event, function() {
+      if (class1==class2)
         obj.toggleClass(class1);
       else if (obj.hasClass(class1))
         obj.removeClass(class1).addClass(class2);
