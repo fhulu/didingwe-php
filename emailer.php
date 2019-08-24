21210faebaedfbcb6170f004082498533bc16106
Fhulu Lidzhade <fhulu@mukoni.co.za>
2019-08-24 23:28:44 +0200

    using latest version of PHPMailer

--- emailer.php
+++ emailer.php
@@ -1,41 +1,56 @@
 <?php
 
-class emailer
-{
-  var $page;
-  var $mime;
-  function __construct($page)
-  {
-    $this->page = $page;
-    set_include_path("./didi/pear");
-    require_once "Mail.php";
-    require_once("Mail/mime.php");
-    $this->mime = new Mail_mime("\n");
-    restore_include_path();
-}
+require_once './didi/module.php';
 
-  function send($options)
-  {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require './didi/PHPMailer/src/Exception.php';
+require './didi/PHPMailer/src/PHPMailer.php';
+require './didi/PHPMailer/src/SMTP.php';
+
+class emailer extends module {
+  function __construct($page) {
+    parent::__construct($page);
+  }
+
+  function send($options) {
     $this->page->merge_context('emailer.send', $options);
-    $headers = $options['headers'];
-    $from = $options['from'];
-    $to = $options['to'];
-    $message = $options['message'];
-    $subject = $options ['subject'];
-    log::debug_json("SENDMAIL from $from to $to SUBJECT $subject", $options);
-    $headers['From'] = $from;
-    $headers['Subject'] = $subject;
-    $headers['To'] = $to;
-
-    $mime = $this->mime;
-    set_include_path("./didi/pear");
-    $mime->setHTMLBody($message);
-    $message = $mime->get();
-    $headers = $mime->headers($headers);
-    $smtp = Mail::factory('smtp',  $options['smtp']);
-    $result = $smtp->send($to, $headers, $message);
-    restore_include_path();
-    if (!$result)
-      return log::error("Error sending email to $to");
+    log::debug_json("EMAIL OPTIONS", $options);
+    $mail = new PHPMailer(true);
+    foreach ($options as $name=>$value) {
+      $capitalized = false;
+      $capital = strtoupper(substr($name, 0, 1)) . substr($name, 1);
+      $setter = "set$capital";
+      $adder = "add$capital";
+      if ( method_exists($mail, $name) || $capitalized = method_exists($mail, $capital)) {
+        if ($capitalized) $name = $capital;
+        if (!is_array($value)) $value = [$value] ;
+        log::debug_json("PHPMAIL.$name", $value);
+        call_user_func_array([$mail, $name], $value);
+      }
+      else if ( method_exists($mail, $setter)) {
+        if (!is_array($value)) $value = [$value] ;
+        log::debug_json("PHPMAIL.$setter", $value);
+        call_user_func_array([$mail, $setter], $value);
+      }
+      else if ( method_exists($mail, $adder)) {
+        if (!is_array($value))
+          $value = [ [$value] ];
+        else if (!is_array($value[0]))
+          $value = [ $value ];
+        foreach($value as $args) {
+          log::debug_json("PHPMAIL.$adder", $args);
+          call_user_func_array([$mail, $adder], $args);
+        }
+      }
+      else if ( property_exists($mail, $name) || $capitalized = property_exists($mail, $capital)) {
+        if ($capitalized) $name = $capital;
+        log::debug_json("PHPMAIL.$name", $value);
+        $mail->$name = $value;
+      }
+    }
+    $mail->send();
   }
 }
