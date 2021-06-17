9936f246ffdb12dc2d87e754a68dc8c8c446e7be
Fhulu Lidzhade <fhulu.lidzhade@metacom.net>
2021-06-17 12:33:07 +0200

    set cookie session timeout

--- session.php
+++ session.php
@@ -1,11 +1,14 @@
 <?php
-session_start();
-
 require_once('utils.php');
 require_once('log.php');
-require_once('db.php');
-
-
+$action = at($_REQUEST, 'action');
+$path = at($_REQUEST, 'path');
+$config = array_merge(['session_timeout'=>15*60], $config);
+$lifetime = $config['session_timeout'];
+ini_set('session.gc_maxlifetime', $lifetime);
+ini_set('session.cookie_lifetime', $lifetime);
+session_set_cookie_params($lifetime);
+session_start(['cookie_lifetime' => $lifetime]);
 
 class session_exception extends Exception {};
 
@@ -54,6 +57,7 @@ class session
     $sql = "insert \$audit_db.session (id, user_id) values ('$session->id','$user->id')";
     log::debug_json("ROLES", $user->roles);
 
+    require_once('db.php');
     global $db;
     $db->insert($sql);
     $_SESSION['instance'] = serialize($session);
@@ -100,6 +104,7 @@ class session
 
   static function force_logout($user_id)
   {
+    require_once('db.php');
     global $db;
     $sql = "update \$audit_db.session set status='C', end_time=now() where user_id = $user_id";
     $db->exec($sql);
@@ -108,6 +113,7 @@ class session
 
   static function logout()
   {
+    require_once('db.php');
     global $db, $session;
     if (isset($db)) {
       $sql = "update \$audit_db.session set status='C', end_time=now() where id = '$session->id'";
@@ -119,6 +125,7 @@ class session
 
   static function verify()
   {
+    require_once('db.php');
     global $session,$db;
     if (is_null($session) || $session->id == '') return false;
 
@@ -136,3 +143,6 @@ if (!isset($daemon_mode)) {
   else
     $session = null;
 }
+
+log::debug_json("USER is", $_SESSION['uid']);
+
