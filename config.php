8ce9374acbe73ad4cb7f3afb2c15139f76c2c8b5
Fhulu Lidzhade <fhulu.lidzhade@metacom.net>
2021-11-05 01:31:21 +0200

    fixed factoring out of spa configuration

--- config.php
+++ config.php
@@ -1,69 +1,49 @@
 <?php
-require_once 'didi/log.php';
-require_once('didi/utils.php');
-
+require_once 'log.php';
+require_once('utils.php');
 
 function do_preprocessing(&$config) {
   $preprocess = $config['preprocess'];
-  if (!$preprocess || !file_exists($preprocess) || in_array($preprocess, get_included_files()) ) return;
-
+  if (!$preprocess || !file_exists($preprocess) || in_array($preprocess, get_included_files()) ) {
+    replace_fields($config,$config);
+    return;
+  }
   require_once($preprocess);
   $func = basename($preprocess, ".php");
   $preprocess_config = $func($config);
   $config = merge_options($config, $preprocess_config);
-#  replace_fields($config,$config,true);
-}
-
-function configure_brand(&$config) {
-  $brand_path = $config['brand_path'];
-  if ($brand_path && file_exists($brand_path)) {
-    $brand_config = load_yaml("$brand_path/app-config.yml", false);
-    if ($brand_config) 
-      $config = merge_options($config, $brand_config);
-    
- #   replace_fields($config, $config, true);
-
-  }
-  else {
-    $brand_path = "./";
-  }
-  $brand_link = ".".$config['brand_name'];
-  $config['brand_link']  = "/$brand_link";
- # if ($brand_path != "./")
- #   replace_fields($config,$config,true);
-
-  if (!file_exists($brand_link))
-    symlink($brand_path,$brand_link);
-  return true;
+  replace_fields($config,$config);
 }
 
 function get_active_config_name()
 {
-  return $_SESSION['auth']? 'authenticated': 'landing';
+  return $_SESSION['auth']? 'auth': 'public';
 }
 
 
 function configure() {
   global $config;
-  $config = load_yaml("didi/app-config.yml", true);
-  $config = merge_options($config, load_yaml("app-config.yml", false));
-
+  $config = merge_options(['didi_root'=>'../didi'], load_yaml("../vocab/app-config.yml", false));
+  $config = merge_options(['didi_path'=>$config['didi_root'] . "/vocab"], $config);
+  $config = merge_options(load_yaml($config['didi_path'] . "/app-config.yml", false), $config);
+  do_preprocessing($config);
   $site_config = load_yaml($config['site_config'], false);
   $config = merge_options($config, $site_config);
-  do_preprocessing($config);
 
-  configure_brand($config);
+  $log = replace_fields($config['log'], $config);
+  if ($log)
+    log::init($log['path'], $log['level']);
 
-  if ($config['log_dir'] && $config['log_file'])
-    log::init($config['log_file'], $config['log_level']);
-
+  $spa = &$config['spa'];
   $active = get_active_config_name();
-  $active_config = &$config[$active];
-  replace_fields($active_config, $_REQUEST, true);
-  replace_fields($active_config, $active_config, true);
+  $active_config = $spa = merge_options($spa, $spa[$active]);
+
+  replace_fields($active_config, $_REQUEST);
+  replace_fields($active_config, $active_config);
   $config = merge_options($config, $active_config);
+  replace_fields($config, $config);
   do_preprocessing($config);
-  replace_fields($config,$config,true);
   $_SESSION['config'] = $config;
   return $site_config != null;
 }
+
--- index.php
+++ index.php
@@ -1,55 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 session_start();
-require_once('log.php');
-require_once('utils.php');
+require_once('config.php');
 
-
-function do_preprocessing(&$config) {
-  $preprocess = $config['preprocess'];
-  if (!$preprocess || !file_exists($preprocess) || in_array($preprocess, get_included_files()) ) {
-    replace_fields($config,$config);
-    return;
-  }
-  require_once($preprocess);
-  $func = basename($preprocess, ".php");
-  $preprocess_config = $func($config);
-  $config = merge_options($config, $preprocess_config);
-  replace_fields($config,$config);
-}
-
-function get_active_config_name()
-{
-  return $_SESSION['auth']? 'auth': 'public';
-}
-
-
-function configure() {
-  global $config;
-  $config = merge_options(['didi_root'=>'../didi'], load_yaml("../vocab/app-config.yml", false));
-  $config = merge_options(['didi_path'=>$config['didi_root'] . "/vocab"], $config);
-  $config = merge_options(load_yaml($config['didi_path'] . "/app-config.yml", false), $config);
-  do_preprocessing($config);
-  $site_config = load_yaml($config['site_config'], false);
-  $config = merge_options($config, $site_config);
-
-  $log = replace_fields($config['log'], $config);
-  if ($log)
-    log::init($log['path'], $log['level']);
-
-  $spa = &$config['spa'];
-  $active = get_active_config_name();
-  $active_config = $spa = merge_options($spa, $spa[$active]);
-
-  replace_fields($active_config, $_REQUEST);
-  replace_fields($active_config, $active_config);
-  $config = merge_options($config, $active_config);
-  replace_fields($config, $config);
-  do_preprocessing($config);
-  $_SESSION['config'] = $config;
-  return $site_config != null;
-}
-
+configure();
 function process_action() {
   if (is_null($_REQUEST['action'])) return false;
   require_once('page.php');
@@ -74,10 +28,6 @@ function process_redirection() {
   return true;
 }
 
-configure();
-log::debug_json("BROWSER REQUEST", $_REQUEST);
-if (process_redirection() || load_default_page() || process_action()) return ;
-
 function build_tag_template_attrs($attrs, $exclusions) {
   $template = "";
   foreach ($attrs as $name=>$value) {
@@ -86,7 +36,6 @@ function build_tag_template_attrs($attrs, $exclusions) {
   }
   return $template;
 }
-
 
 function build_tag_lines($tag_type) {
   global $config;
@@ -128,6 +77,8 @@ function build_tag_lines($tag_type) {
   $config[$tag_type] = $lines;
 }
 
+log::debug_json("BROWSER REQUEST", $_REQUEST);
+if (process_redirection() || load_default_page() || process_action()) return ;
 
 $spa = $config['spa'];
 $active = get_active_config_name();
@@ -139,6 +90,7 @@ if (isset($request['path']))
 else
   $request['content'] = $active_config['content'];
 unset($request['path']);
+
 $page =  $request['page'];
 if (!isset($page)) $page = $active_config['page'];
 $options = ["path"=>$page, 'request'=>$request, 'request_method'=>$config['request_method']];
